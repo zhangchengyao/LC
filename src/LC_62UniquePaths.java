818fcf59a25712a9de3d044546fa6ab995c44baf
chengyaoZhang <tc2002618@126.com>
2018-05-24 22:34:01 +0800

    LC62 update

--- src/LC_62UniquePaths.java
+++ src/LC_62UniquePaths.java
@@ -1,6 +1,19 @@
 public class LC_62UniquePaths {
     public int uniquePaths(int m, int n) {
         if(m==1 || n==1) return 1;
-        return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+        long numerator = 1;
+        long denominator = 1;
+        int a = m + n -2;
+        int b = Math.min(m-1, n-1);
+        int diff = a - b + 1;
+        while(a>=diff){
+            numerator *= a;
+            a--;
+        }
+        while(b>=1){
+            denominator *= b;
+            b--;
+        }
+        return (int)(numerator/denominator);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args){
         LC_62UniquePaths up = new LC_62UniquePaths();
-        System.out.println(up.uniquePaths(7, 3));
+        System.out.println(up.uniquePaths(100, 3));
     }
 }
