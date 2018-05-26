f4354413b49de24bf09d6b1ee44aae372deafb96
chengyaoZhang <tc2002618@126.com>
2018-05-26 18:16:24 +0800

    LC217

---
+++ src/LC217_ContainsDuplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC217_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])) return true;
+            else map.put(nums[i], 1);
+        }
+        return false;
+    }
+}
