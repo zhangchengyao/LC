e28b5030e07cdd561ec86cc48133bb00dcfca4db
chengyaoZhang <tc2002618@126.com>
2018-06-19 23:36:19 +0800

    LC381

--- src/LC381_InsertDeleteGetRandomO1DuplicatesAllowed.java
+++ src/LC381_InsertDeleteGetRandomO1DuplicatesAllowed.java
@@ -1,3 +1,54 @@
-public class LC381_InsertDeleteGetRandomO1DuplicatesAllowed {
+import java.util.*;
 
+public class LC381_InsertDeleteGetRandomO1DuplicatesAllowed {
+    Map<Integer, Set<Integer>> locs;
+    List<Integer> nums;
+    /** Initialize your data structure here. */
+    public LC381_InsertDeleteGetRandomO1DuplicatesAllowed() {
+        locs = new HashMap<>();
+        nums = new ArrayList<>();
+    }
+
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        nums.add(val);
+        if(!locs.containsKey(val)){
+            locs.put(val, new HashSet<>());
+            locs.get(val).add(nums.size()-1);
+            return true;
+        }else{
+            // already contains this element
+            locs.get(val).add(nums.size()-1);
+            return false;
+        }
+    }
+
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if(locs.containsKey(val)){
+            int index = locs.get(val).iterator().next();
+            int lastElement = nums.get(nums.size()-1);
+            if(index!=nums.size()-1){
+                // the element is not the last element
+                // then swap it with the last element
+
+                locs.get(lastElement).add(index);
+                locs.get(lastElement).remove(nums.size()-1);
+                nums.set(index, lastElement);
+            }
+            if(val!=lastElement) locs.get(val).remove(index);
+            if(locs.get(val).isEmpty()){
+                locs.remove(val);
+            }
+            nums.remove(nums.size()-1);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return nums.get(new Random().nextInt(nums.size()));
+    }
 }
