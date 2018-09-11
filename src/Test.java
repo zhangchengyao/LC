9d3e4ae39dc410a6244377b39db9b50408d18548
chengyaoZhang <tc2002618@126.com>
2018-09-11 13:16:08 +0800

    LC209

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 public class Test {
     public static void main(String[] args){
-        LC80_RemoveDuplicatesFromSortedArrayII lc = new LC80_RemoveDuplicatesFromSortedArrayII();
-        int[] arr = new int[]{0,0,1,1,1,1,2,3,3};
-        int len = lc.removeDuplicates(arr);
+        LC209_MinimumSizeSubarraySum lc = new LC209_MinimumSizeSubarraySum();
+        int[] arr = new int[]{1,2,3,4,5};
+        int len = lc.minSubArrayLen(15,arr);
+        System.out.println(len);
     }
 }
