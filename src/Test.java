58ae4db90e4d157c265385e4478eab758b3f42c0
chengyaoZhang <tc2002618@126.com>
2018-09-16 15:52:45 -0700

    LC401

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args){
-        LC326_PowerOfThree lc = new LC326_PowerOfThree();
-        System.out.println(lc.isPowerOfThree(82));
+        LC401_BinaryWatch lc = new LC401_BinaryWatch();
+        System.out.println(lc.readBinaryWatch(1).size());
     }
 }
