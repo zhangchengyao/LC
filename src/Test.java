b1665bd8e858355132c01308136b41f7dffa8d23
chengyaoZhang <tc2002618@126.com>
2018-09-16 13:04:15 -0700

    LC338

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,6 @@
 public class Test {
     public static void main(String[] args){
-        LC209_MinimumSizeSubarraySum lc = new LC209_MinimumSizeSubarraySum();
-        int[] arr = new int[]{1,2,3,4,5};
-        int len = lc.minSubArrayLen(15,arr);
-        System.out.println(len);
+        LC338_CountingBits lc = new LC338_CountingBits();
+
     }
 }
