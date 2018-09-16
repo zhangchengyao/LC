45ee34f9181abcc5975a84de442d698567a6e607
chengyaoZhang <tc2002618@126.com>
2018-09-16 14:03:30 -0700

    LC190

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args){
-        LC338_CountingBits lc = new LC338_CountingBits();
+        LC190_ReverseBits lc = new LC190_ReverseBits();
+        System.out.println(lc.reverseBits(1));
 
     }
 }
