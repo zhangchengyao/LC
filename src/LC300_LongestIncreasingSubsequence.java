f506db32aeb0990bcbd35c01045a2abcb135cb3c
chengyaoZhang <tc2002618@126.com>
2018-09-16 12:08:54 -0700

    LC300 modify

--- src/LC300_LongestIncreasingSubsequence.java
+++ src/LC300_LongestIncreasingSubsequence.java
@@ -2,11 +2,11 @@ public class LC300_LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
         if(nums.length==0) return 0;
         if(nums.length==1) return 1;
-        int[] dp = new int[nums.length+1];
-        dp[1] = 1;
-        for(int i=2;i<dp.length;i++){
-            for(int j=0;j<i-1;j++){
-                if(nums[i-1]>nums[j]) dp[i] = Math.max(dp[i], dp[j+1]+1);
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]) dp[i] = Math.max(dp[i], dp[j]+1);
                 else dp[i] = Math.max(dp[i], 1);
             }
         }
