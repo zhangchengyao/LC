5bcfcdd5065058f300aff060db710da9c7ef2550
chengyaoZhang <tc2002618@126.com>
2018-09-20 14:14:22 -0700

    LC416

--- src/LC416_PartitionEqualSubsetSum.java
+++ src/LC416_PartitionEqualSubsetSum.java
@@ -8,15 +8,34 @@ public class LC416_PartitionEqualSubsetSum {
         }
         int half = sum/2;
         if(half*2!=sum) return false;
-        return check(nums, nums.length-1, half, new HashMap<>());
+
+        boolean[] dp = new boolean[half+1];
+        dp[0] = true;
+        for(int i=0;i<nums.length;i++){
+            for(int j=dp.length-1;j>=nums[i];j--){
+                dp[j] = dp[j] || dp[j-nums[i]];
+            }
+        }
+        return dp[half];
     }
-    private boolean check(int[] nums, int end, int sum, HashMap<String, Boolean> map){
-        String state = end+" "+sum;
-        if(map.containsKey(state)) return map.get(state);
-        if(sum<=0) return false;
-        if(end==0) return nums[end]==sum;
-        boolean res = check(nums, end-1, sum, map)||check(nums, end-1, sum-nums[end], map);
-        map.put(state, res);
-        return res;
-    }
+//    public boolean canPartition(int[] nums) {
+//        if(nums.length<2) return false;
+//        int sum = 0;
+//        for(int i=0;i<nums.length;i++){
+//            sum += nums[i];
+//        }
+//        int half = sum/2;
+//        if(half*2!=sum) return false;
+//
+//        boolean[][] dp = new boolean[nums.length][half+1];
+//        for(int i=0;i<nums.length;i++) dp[i][0] = true;
+//        dp[0][nums[0]] = true;
+//        for(int i=1;i<nums.length;i++){
+//            for(int j=1;j<=half;j++){
+//                if(nums[i]>j) dp[i][j] = dp[i-1][j];
+//                else dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
+//            }
+//        }
+//        return dp[nums.length-1][half];
+//    }
 }
