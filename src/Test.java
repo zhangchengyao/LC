9bcf34ee5613a4613c862e024245c21b38bb30b9
chengyaoZhang <tc2002618@126.com>
2018-09-20 10:44:37 -0700

    LC446

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-        LC467_UniqueSubstringsInWraparoundString lc = new LC467_UniqueSubstringsInWraparoundString();
-        System.out.println(lc.findSubstringInWraproundString("abaab"));
+        LC446_ArithmeticSlicesIISubsequence lc = new LC446_ArithmeticSlicesIISubsequence();
+        System.out.println(lc.numberOfArithmeticSlices(new int[]{1,7,7,13,19}));
     }
 }
