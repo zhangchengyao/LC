4fd0f4979a807282cc1f29fa64ab7ca3fd1736a0
chengyaoZhang <tc2002618@126.com>
2018-09-19 22:10:44 -0700

    LC467 modify

--- src/LC467_UniqueSubstringsInWraparoundString.java
+++ src/LC467_UniqueSubstringsInWraparoundString.java
@@ -2,31 +2,20 @@ import java.util.HashSet;
 
 public class LC467_UniqueSubstringsInWraparoundString {
     public int findSubstringInWraproundString(String p) {
-        int[][] dp = new int[p.length()][p.length()];
-        HashSet<String> set = new HashSet<>();
+        int[] dp = new int[p.length()];
+        int[] maxLen = new int[26];
+        for(int i=p.length()-1;i>=0;i--){
+            if((i==p.length()-1) || (p.charAt(i)+1!=p.charAt(i+1) && !(p.charAt(i)=='z' && p.charAt(i+1)=='a'))){
+                dp[i] = 1;
+            }else{
+                dp[i] = dp[i+1]+1;
+            }
+        }
+        for(int i=0;i<dp.length;i++){
+            if(dp[i]>maxLen[p.charAt(i)-'a']) maxLen[p.charAt(i)-'a'] = dp[i];
+        }
         int res = 0;
-        for(int i=0;i<dp.length;i++){
-            dp[i][i] = 1;
-            if(!set.contains(p.substring(i, i+1))){
-                set.add(p.substring(i, i+1));
-                res++;
-            }
-        }
-        for(int i=0;i<dp.length;i++){
-            for(int j=i+1;j<dp.length;j++){
-                if(dp[i][j-1]==1){
-                    if((p.charAt(j)==(p.charAt(j-1)+1)) || (p.charAt(j)=='a' && p.charAt(j-1)=='z')){
-                        dp[i][j] = 1;
-                        if(!set.contains(p.substring(i, j+1))){
-                            set.add(p.substring(i, j+1));
-                            res++;
-                        }
-                    }
-                }else{
-                    break;
-                }
-            }
-        }
+        for(int i=0;i<maxLen.length;i++) res += maxLen[i];
         return res;
     }
 }
