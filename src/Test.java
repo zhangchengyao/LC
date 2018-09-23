f2028dd0d8772ef5ea570055fa31c3f752d3a8f7
chengyaoZhang <tc2002618@126.com>
2018-09-23 10:08:04 -0700

    LC717

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-        LC746_MinCostClimbingStairs lc = new LC746_MinCostClimbingStairs();
-        System.out.println(lc.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+        LC717_1BitAnd2BitCharacters lc = new LC717_1BitAnd2BitCharacters();
+        lc.isOneBitCharacter(new int[]{1,1,1,0});
     }
 }
