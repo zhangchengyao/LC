f9185101222bcfc665673c863327b3c220834a5e
chengyaoZhang <tc2002618@126.com>
2018-09-24 16:55:59 -0700

    LC389

--- src/LC389_FindTheDifference.java
+++ src/LC389_FindTheDifference.java
@@ -1,6 +1,15 @@
 import java.util.HashMap;
 
 public class LC389_FindTheDifference {
+    // an super fast algorithm using xor
+//    public char findTheDifference(String s, String t) {
+//        char res = t.charAt(0);
+//        for(int i=0;i<s.length();i++){
+//            res ^= s.charAt(i);
+//            res ^= t.charAt(i+1);
+//        }
+//        return res;
+//    }
     public char findTheDifference(String s, String t) {
         HashMap<Character, Integer> map = new HashMap<>();
         char c = ' ';
