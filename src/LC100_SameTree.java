e7f6735afa3ea766c8a33766a765bdbdd7453b25
chengyaoZhang <tc2002618@126.com>
2018-09-24 21:03:59 -0700

    LC100

---
+++ src/LC100_SameTree.java
@@ -0,0 +1,14 @@
+public class LC100_SameTree {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){ val = x; }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null &&q==null) return true;
+        if((p==null && q!=null) || (p!=null && q==null)) return false;
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
