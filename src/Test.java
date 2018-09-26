2532113d77d3c26d1c53e980fa27034ca818e498
chengyaoZhang <tc2002618@126.com>
2018-09-25 20:48:52 -0700

    LC236

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-        LC405_ConvertANumberToHexadecimal lc = new LC405_ConvertANumberToHexadecimal();
-        System.out.println(lc.toHex(-1));
+
     }
 }
