619b39e4c0123ff8b0faf18b83f4c69b20e8d951
chengyaoZhang <tc2002618@126.com>
2018-09-25 19:16:48 -0700

    LC405

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-        LC387_FirstUniqueCharacterInAString lc = new LC387_FirstUniqueCharacterInAString();
-        lc.firstUniqChar("leetcode");
+        LC405_ConvertANumberToHexadecimal lc = new LC405_ConvertANumberToHexadecimal();
+        System.out.println(lc.toHex(-1));
     }
 }
