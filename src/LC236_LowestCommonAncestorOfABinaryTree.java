c282cd64899c21a44b131bd2ea23ab983e7e47f5
chengyaoZhang <tc2002618@126.com>
2018-09-25 20:55:57 -0700

    LC236 modify

--- src/LC236_LowestCommonAncestorOfABinaryTree.java
+++ src/LC236_LowestCommonAncestorOfABinaryTree.java
@@ -7,6 +7,14 @@ public class LC236_LowestCommonAncestorOfABinaryTree {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+    /* a better solution */
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root==null || root==p || root==q) return root;
+//        TreeNode left = lowestCommonAncestor(root.left, p, q);
+//        TreeNode right = lowestCommonAncestor(root.right, p, q);
+//        if(left!=null && right!=null) return root;
+//        else return left==null?right:left;
+//    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         HashMap<String, Boolean> map = new HashMap<>();
         TreeNode cur = root;
