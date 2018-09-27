b689c2429fa15891b9ecf2c6c20b4109bb08a8e9
chengyaoZhang <tc2002618@126.com>
2018-09-27 14:59:10 -0700

    LC382

--- src/LC384_ShuffleAnArray.java
+++ src/LC384_ShuffleAnArray.java
@@ -13,15 +13,13 @@ public class LC384_ShuffleAnArray {
 
     /** Returns a random shuffling of the array. */
     public int[] shuffle() {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0;i<data.length;i++){
-            list.add(data[i]);
-        }
         int[] res = new int[data.length];
+        for(int i=0;i<data.length;i++) res[i] = data[i];
         for(int i=0;i<data.length;i++){
-            int index = (int)(list.size()*Math.random());
-            res[i] = list.get(index);
-            list.remove(index);
+            int index = (int)(res.length*Math.random());
+            int tmp = res[index];
+            res[index] = res[i];
+            res[i] = tmp;
         }
         return res;
     }
