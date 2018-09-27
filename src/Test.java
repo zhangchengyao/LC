c90988f48febf91559ab6b4604b3126827e2745a
chengyaoZhang <tc2002618@126.com>
2018-09-26 21:07:52 -0700

    LC400

--- src/Test.java
+++ src/Test.java
@@ -2,18 +2,7 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args){
-        LC155_MinStack obj = new LC155_MinStack();
-         obj.push(512);
-         obj.push(-1024);
-         obj.push(-1024);
-         obj.push(512);
-         obj.pop();
-         obj.show();
-        System.out.println();
-         obj.pop();
-         obj.show();
-        System.out.println();
-         obj.pop();
-         obj.show();
+        LC400_NthDigit lc = new LC400_NthDigit();
+        System.out.println(lc.findNthDigit(1000000000));;
     }
 }
