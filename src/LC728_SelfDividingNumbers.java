afc471c00f617d58ae208af093e37f2d9ed1fa4a
chengyaoZhang <tc2002618@126.com>
2018-09-26 21:31:53 -0700

    LC728

---
+++ src/LC728_SelfDividingNumbers.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC728_SelfDividingNumbers {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(check(i)) res.add(i);
+        }
+        return res;
+    }
+    private boolean check(int num){
+        int n;
+        int tmp = num;
+        while(tmp!=0){
+            n = tmp%10;
+            if(n==0 || num%n!=0) return false;
+            tmp /= 10;
+        }
+        return true;
+    }
+}
