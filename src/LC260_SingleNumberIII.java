988fee5ddca76b0a7fc8cc8cdddcaf40098cda01
chengyaoZhang <tc2002618@126.com>
2018-09-27 13:37:34 -0700

    LC260

--- src/LC260_SingleNumberIII.java
+++ src/LC260_SingleNumberIII.java
@@ -1,6 +1,20 @@
 import java.util.ArrayList;
 
 public class LC260_SingleNumberIII {
+    /* a genius bit manipulation solution */
+//    public int[] singleNumber(int[] nums) {
+//        int[] res = new int[2];
+//        int xor = 0;
+//        for(int i=0;i<nums.length;i++){
+//            xor ^= nums[i];
+//        }
+//        xor &= -xor;
+//        for(int i=0;i<nums.length;i++){
+//            if((xor & nums[i])==0) res[0] ^= nums[i];
+//            else res[1] ^= nums[i];
+//        }
+//        return res;
+//    }
     public int[] singleNumber(int[] nums) {
         ArrayList<Integer> res = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
