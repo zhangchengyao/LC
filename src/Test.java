c5e8e8570056f9c3a7c5d3098510d208bd9cbac8
chengyaoZhang <tc2002618@126.com>
2018-09-28 16:17:09 -0700

    LC481

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        LC241_DifferentWaysToAddParentheses lc = new LC241_DifferentWaysToAddParentheses();
-        List<Integer> res = lc.diffWaysToCompute("2*3-4*5");
-//        for(int i:res){
-//            System.out.println(i);
-//        }
+        LC481_MagicalString lc = new LC481_MagicalString();
+        System.out.println(lc.magicalString(10));
     }
 }
