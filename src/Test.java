8e53a771536751e809ffc794add27f481b831d3e
chengyaoZhang <tc2002618@126.com>
2018-09-29 11:13:43 -0700

    LC43

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        LC415_AddStrings lc  = new LC415_AddStrings();
-        System.out.println(lc.addStrings("9", "1"));
+        LC43_MultiplyStrings lc = new LC43_MultiplyStrings();
+        System.out.println(lc.multiply("1237", "456"));
     }
 }
