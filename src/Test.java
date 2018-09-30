d0bdcae7f02584a31248e59934bb0f1cc8dc860e
chengyaoZhang <tc2002618@126.com>
2018-09-30 15:14:34 -0700

    LC402

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        LC151_ReverseWordsInAString lc = new LC151_ReverseWordsInAString();
-        System.out.println(lc.reverseWords("the sky is blue  "));
+        LC402_RemoveKDigits lc = new LC402_RemoveKDigits();
+        System.out.println(lc.removeKdigits("1432219", 3));
     }
 }
