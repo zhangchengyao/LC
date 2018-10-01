e41ad9f7172b7d18466301728b02ed6e1bef0f19
chengyaoZhang <tc2002618@126.com>
2018-09-30 23:40:49 -0700

    LC526

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        LC402_RemoveKDigits lc = new LC402_RemoveKDigits();
-        System.out.println(lc.removeKdigits("1432219", 3));
+        LC526_BeautifulArrangement lc = new LC526_BeautifulArrangement();
+        System.out.println(lc.countArrangement(3));
     }
 }
