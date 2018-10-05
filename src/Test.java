92b0e94453bc847675afcbe1b7c67b3e35dc9477
chengyaoZhang <tc2002618@126.com>
2018-10-04 19:39:53 -0700

    LC10

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        LC730_CountDifferentPalindromicSubsequences lc = new LC730_CountDifferentPalindromicSubsequences();
-        System.out.println(lc.countPalindromicSubsequences("bcbacbabdcbcbdcbddcaaccdcbbcdbcabbcdddadaadddbdbbbdacbabaabdddcaccccdccdbabcddbdcccabccbbcdbcdbdaada"));
+        LC10_RegularExpressionMatching lc = new LC10_RegularExpressionMatching();
+        System.out.println(lc.isMatch("", ""));
     }
 }
