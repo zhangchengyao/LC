5ca1f5900daf7b529136da217877bc8688c1dc2e
chengyaoZhang <tc2002618@126.com>
2018-10-08 20:58:14 -0700

    LC685

--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,12 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args){
-        LC249_GroupShiftedStrings lc = new LC249_GroupShiftedStrings();
-        List<List<String>> res = lc.groupStrings(new String[]{"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"});
-        for(List<String> line: res){
-            for(String s: line){
-                System.out.print(s+" ");
-            }
-            System.out.println();
-        }
+        LC685_RedundantConnectionII LC = new LC685_RedundantConnectionII();
+        int[][] input = new int[4][2];
+        input[0] = new int[]{2,1};
+        input[1] = new int[]{3,1};
+        input[2] = new int[]{4,2};
+        input[3] = new int[]{1,4};
+        System.out.println(LC.findRedundantDirectedConnection(input)[0]);;
     }
 }
