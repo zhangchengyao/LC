17c5456f2534fa8416e0261d717a3fc1d8a85fef
chengyaoZhang <tc2002618@126.com>
2018-10-10 10:30:18 -0700

    LC659

--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,7 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args){
-        LC685_RedundantConnectionII LC = new LC685_RedundantConnectionII();
-        int[][] input = new int[4][2];
-        input[0] = new int[]{2,1};
-        input[1] = new int[]{3,1};
-        input[2] = new int[]{4,2};
-        input[3] = new int[]{1,4};
-        System.out.println(LC.findRedundantDirectedConnection(input)[0]);;
+        LC659_SplitArrayIntoConsecutiveSubsequences lc = new LC659_SplitArrayIntoConsecutiveSubsequences();
+        System.out.println(lc.isPossible(new int[]{1,2,3,3,4,4,5,5}));
     }
 }
