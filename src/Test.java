f3508b931a5919d58e88ea6205555451942da5ee
chengyaoZhang <tc2002618@126.com>
2018-10-11 18:48:43 -0700

    LC727

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args){
-        LC659_SplitArrayIntoConsecutiveSubsequences lc = new LC659_SplitArrayIntoConsecutiveSubsequences();
-        System.out.println(lc.isPossible(new int[]{1,2,3,3,4,4,5,5}));
+        LC727_MinimumWindowSubsequence lc = new LC727_MinimumWindowSubsequence();
+        System.out.println(lc.minWindow("cnhczmccqouqadqtmjjzl", "dq"));
     }
 }
