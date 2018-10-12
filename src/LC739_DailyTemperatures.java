f29fcfddbfd2d1ad168aab362bec5766fdea0204
chengyaoZhang <tc2002618@126.com>
2018-10-11 22:35:25 -0700

    LC739

---
+++ src/LC739_DailyTemperatures.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class LC739_DailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        // descending stack
+        int[] res = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=T.length-1;i>=0;i--){
+            while(!stack.isEmpty() && T[stack.peek()]<=T[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()) res[i] = 0;
+            else res[i] = stack.peek()-i;
+            stack.push(i);
+        }
+        return res;
+    }
+}
