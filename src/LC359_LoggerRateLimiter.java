eebc84dda79d2ba3180dab1e7de97b8135f6bebe
chengyaoZhang <tc2002618@126.com>
2018-10-18 15:18:59 -0700

    LC359

---
+++ src/LC359_LoggerRateLimiter.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class LC359_LoggerRateLimiter {
+    HashMap<String, Integer> map;
+    /** Initialize your data structure here. */
+    public LC359_LoggerRateLimiter() {
+        map = new HashMap<>();
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     If this method returns false, the message will not be printed.
+     The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message) || map.get(message)<=timestamp-10){
+            map.put(message, timestamp);
+            return true;
+        }
+        else return false;
+    }
+}
