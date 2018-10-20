867474874727c8efe60dc87711fcf3b2876a4dc8
chengyaoZhang <tc2002618@126.com>
2018-10-19 21:57:31 -0700

    LC803

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ import java.util.List;
 import java.util.TreeSet;
 
 public class Test {
-    public static void main(String[] args){
-        LC844_BackspaceStringCompare lc = new LC844_BackspaceStringCompare();
-        System.out.println(lc.backspaceCompare("a#c", "b"));
+    public static void main(String[] args) {
+//        LC844_BackspaceStringCompare lc = new LC844_BackspaceStringCompare();
+//        System.out.println(lc.backspaceCompare("a#c", "b"));
     }
 }
