8288526dea35e0065365b6c3c76d14599c8d60b7
chengyaoZhang <tc2002618@126.com>
2018-10-21 09:24:35 -0700

    LC850

--- src/LC753_CrackingTheSafe.java
+++ src/LC753_CrackingTheSafe.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class LC753_CrackingTheSafe {
-    HashMap<String, Boolean> map = new HashMap<>();
     HashSet<String> set = new HashSet<>();
     public String crackSafe(int n, int k) {
         StringBuilder res = new StringBuilder();
@@ -12,7 +11,6 @@ public class LC753_CrackingTheSafe {
     private boolean check(StringBuilder res, int n, int k){
         if(res.length()==((int)Math.pow(k, n)+n-1)) return true;
         String s = res.toString();
-        if(map.containsKey(s)) return map.get(s);
         for(int i=0;i<k;i++){
             res.append(i);
             if(res.length()>=n){
@@ -24,14 +22,12 @@ public class LC753_CrackingTheSafe {
             }
             boolean suc = check(res, n, k);
             if(suc){
-                map.put(res.toString(), true);
                 return true;
             }else{
                 set.remove(res.substring(res.length()-n));
                 res.deleteCharAt(res.length()-1);
             }
         }
-        map.put(res.toString(), false);
         return false;
     }
 }
