3d32b2542fb6c166c545d25e37c4416a50a449ce
chengyaoZhang <tc2002618@126.com>
2018-10-23 17:16:52 -0700

    LC159

--- src/LC159_LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ src/LC159_LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,3 +1,21 @@
 public class LC159_LongestSubstringWithAtMostTwoDistinctCharacters {
-
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int[] map = new int[256];
+        int cnt = 0;
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        while(right<s.length()){
+            if(map[s.charAt(right++)]++==0) cnt++;
+            if(cnt>2){
+                while(cnt>2){
+                    if(map[s.charAt(left)]--==1) cnt--;
+                    left++;
+                }
+            }
+            if(cnt==2) res = Math.max(res, right-left);
+        }
+        res = Math.max(res, right-left);
+        return res;
+    }
 }
