f78e7db0933acb1ffe84f81a3e136a29e9cff0bd
chengyaoZhang <tc2002618@126.com>
2018-10-24 10:47:57 -0700

    LC254

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        LC681_NextClosestTime lc = new LC681_NextClosestTime();
-        System.out.println(lc.nextClosestTime("13:55"));
+        LC254_FactorCombinations lc = new LC254_FactorCombinations();
+        System.out.println(lc.getFactors(32));
     }
 }
