97e362e645542ef393f8e2937c40647d1834acab
chengyaoZhang <tc2002618@126.com>
2018-10-23 17:09:22 -0700

    LC681

---
+++ src/LC159_LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,3 @@
+public class LC159_LongestSubstringWithAtMostTwoDistinctCharacters {
+
+}
--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 
 public class Test {
     public static void main(String[] args) {
-        LC457_CircularArrayLoop lc = new LC457_CircularArrayLoop();
-        System.out.println(lc.circularArrayLoop(new int[]{-2, 1, -1, -2, -2}));
+        LC681_NextClosestTime lc = new LC681_NextClosestTime();
+        System.out.println(lc.nextClosestTime("13:55"));
     }
 }
