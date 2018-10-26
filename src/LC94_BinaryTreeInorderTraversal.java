59ad50e8e6734f5c6aa72d410a0ad3175c6917e3
chengyaoZhang <tc2002618@126.com>
2018-10-25 20:49:33 -0700

    LC94 Morris inorder traversal

--- src/LC94_BinaryTreeInorderTraversal.java
+++ src/LC94_BinaryTreeInorderTraversal.java
@@ -9,6 +9,32 @@ public class LC94_BinaryTreeInorderTraversal {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+    // Morris traversal
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<>();
+//        TreeNode cur = root;
+//        TreeNode prev = null;
+//        while(cur!=null){
+//            if(cur.left==null){
+//                res.add(cur.val);
+//                cur = cur.right;
+//            }
+//            else{
+//                prev = cur.left;
+//                while(prev.right!=null && prev.right!=cur) prev = prev.right;
+//                if(prev.right==null){
+//                    prev.right = cur;
+//                    cur = cur.left;
+//                }
+//                else{
+//                    prev.right = null;
+//                    res.add(cur.val);
+//                    cur = cur.right;
+//                }
+//            }
+//        }
+//        return res;
+//    }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         TreeNode cur = root;
