ef6e4903fa744c5070cdb75cdced6a2a147375da
chengyaoZhang <tc2002618@126.com>
2018-10-27 17:55:11 -0700

    LC122

---
+++ src/LC122_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,9 @@
+public class LC122_BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1]) maxProfit += prices[i+1]-prices[i];
+        }
+        return maxProfit;
+    }
+}
