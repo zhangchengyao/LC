31117db0e13e44c7ad6fb1ad1ca7be9e9b4716fa
chengyaoZhang <tc2002618@126.com>
2018-11-01 09:30:59 -0700

    LC722

--- src/LC722_RemoveComments.java
+++ src/LC722_RemoveComments.java
@@ -1,3 +1,37 @@
-public class LC722_RemoveComments {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LC722_RemoveComments {
+    public List<String> removeComments(String[] source) {
+        List<String> res = new ArrayList<>();
+        boolean block = false;
+        StringBuilder sb = new StringBuilder();
+        for(String src: source){
+            for(int i=0;i<src.length();i++){
+                if(block){
+                    if(i<src.length()-1 && src.substring(i, i+2).equals("*/")){
+                        block = false;
+                        i++;
+                    }
+                }
+                else{
+                    if(i<src.length()-1 && src.substring(i, i+2).equals("//")){
+                        break;
+                    }
+                    else if(i<src.length()-1 && src.substring(i, i+2).equals("/*")){
+                        block = true;
+                        i++;
+                    }
+                    else{
+                        sb.append(src.charAt(i));
+                    }
+                }
+            }
+            if(!block && sb.length()>0){
+                res.add(sb.toString());
+                sb = new StringBuilder();
+            }
+        }
+        return res;
+    }
 }
