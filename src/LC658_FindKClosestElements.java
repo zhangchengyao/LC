37e35b01cb24dbbc2a9f675026ed0a510c82fe1a
chengyaoZhang <tc2002618@126.com>
2018-11-16 17:07:52 -0800

    LC658

---
+++ src/LC658_FindKClosestElements.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC658_FindKClosestElements {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        int idx = Arrays.binarySearch(arr, x);
+        if(idx<0) idx = -idx-1;
+        if(idx>0 && x-arr[idx-1]<arr[idx]-x) idx--;
+        res.add(arr[idx]);
+        int i = idx-1;
+        int j = idx+1;
+        for(int cnt=1;cnt<k;cnt++){
+            if(i>=0 && (j>=arr.length || x-arr[i]<=arr[j]-x)) res.add(0, arr[i--]);
+            else res.add(arr[j++]);
+        }
+        return res;
+    }
+}
