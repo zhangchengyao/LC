18b9ebd0f66007af598f6b35cc0cc0c4a90ee922
chengyaoZhang <tc2002618@126.com>
2018-11-20 22:55:15 -0800

    LC738

--- src/LC765_CouplesHoldingHands.java
+++ src/LC765_CouplesHoldingHands.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class LC765_CouplesHoldingHands {
-    // greedy algorithm, but why works?
+    // todo greedy algorithm, but why works?
     public int minSwapsCouples(int[] row) {
         HashMap<Integer, Integer> map = new HashMap<>(); // label => index
         for(int i=0;i<row.length;i++) map.put(row[i], i);
@@ -25,4 +25,34 @@ public class LC765_CouplesHoldingHands {
         }
         return res;
     }
+    // similar to counting sort
+//    public int minSwapsCouples(int[] row) {
+//        int res = 0, N = row.length;
+//
+//        int[] ptn = new int[N];
+//        int[] pos = new int[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            ptn[i] = (i % 2 == 0 ? i + 1 : i - 1);
+//            pos[row[i]] = i;
+//        }
+//
+//        for (int i = 0; i < N; i++) {
+//            int j = ptn[pos[ptn[row[i]]]];
+//            while(i!=j) {
+//                swap(row, i, j);
+//                swap(pos, row[i], row[j]);
+//                res++;
+//                j = ptn[pos[ptn[row[i]]]];
+//            }
+//        }
+//
+//        return res;
+//    }
+//
+//    private void swap(int[] arr, int i, int j) {
+//        int t = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = t;
+//    }
 }
