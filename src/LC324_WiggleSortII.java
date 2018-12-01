f1066b45adb3064c88c103b6fbdff7290edd95f6
chengyaoZhang <tc2002618@126.com>
2018-11-30 21:06:30 -0800

    LC324 modify

--- src/LC324_WiggleSortII.java
+++ src/LC324_WiggleSortII.java
@@ -1,7 +1,34 @@
 import java.util.Arrays;
 
 public class LC324_WiggleSortII {
-    // todo time: O(n) space: O(1)
+    // space: O(1)
+//    public void wiggleSort(int[] nums) {
+//        int n = nums.length;
+//        int median = findKthLargest(nums, (n+1)/2);
+//        int left = 0;
+//        int right = n-1;
+//        int i = 0;
+//        while(i<=right){
+//            int idx = newIndex(i, n);
+//            if(nums[idx]>median){
+//                swap(nums, newIndex(left++, n), idx);
+//                i++;
+//            }else if(nums[idx]<median){
+//                swap(nums, newIndex(right--, n), idx);
+//            }else{
+//                i++;
+//            }
+//        }
+//    }
+//    private int newIndex(int i, int n){
+//        return (2*i+1)%(n|1);
+//    }
+//    private void swap(int[] nums, int i, int j){
+//        int tmp = nums[i];
+//        nums[i] = nums[j];
+//        nums[j] = tmp;
+//    }
+
     // time: O(nlogn), space: O(n)
     public void wiggleSort(int[] nums) {
         int[] tmp = new int[nums.length];
