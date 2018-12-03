8a55ae382327020d6165f6489b34ed804fa8c994
chengyaoZhang <tc2002618@126.com>
2018-12-02 21:03:46 -0800

    LC233 modify

--- src/LC233_NumberOfDigitOne.java
+++ src/LC233_NumberOfDigitOne.java
@@ -1,13 +1,26 @@
 public class LC233_NumberOfDigitOne {
-    // todo difficult to understand
+    // understandable solution
     public int countDigitOne(int n) {
-        long a, b;
         int res = 0;
-        for(long m=1;m<=n;m*=10){
-            a = n/m;
-            b = n%m;
-            res += ((a+8)/10*m + (a%10==1?b+1:0));
+        for(long factor = 1;factor<=n;factor*=10){
+            long cur = (n/factor) %10 ;
+            long low = n % factor;
+            long high = n / (factor*10);
+            if(cur<1) res += high*factor;
+            else if(cur==1) res += high*factor+low+1;
+            else res += (high+1)*factor;
         }
         return res;
     }
+
+//    public int countDigitOne(int n) {
+//        long a, b;
+//        int res = 0;
+//        for(long m=1;m<=n;m*=10){
+//            a = n/m;
+//            b = n%m;
+//            res += ((a+8)/10*m + (a%10==1?b+1:0));
+//        }
+//        return res;
+//    }
 }
