e7d79f3aef045c1d21ba44651c41fbfea021dc2c
chengyaoZhang <tc2002618@126.com>
2018-12-03 23:47:58 -0800

    LC283 modify

--- src/LC283_MoveZeroes.java
+++ src/LC283_MoveZeroes.java
@@ -1,17 +1,26 @@
 public class LC283_MoveZeroes {
     public void moveZeroes(int[] nums) {
-        int num = 0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==0){
-                for(int j=i;j<nums.length-1;j++){
-                    int tmp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = tmp;
-                }
-                i--;
-            }
-            num++;
-            if(num==nums.length) break;
+        int n = nums.length;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) nums[cnt++] = nums[i];
         }
+        for(int i=cnt;i<n;i++) nums[i] = 0;
     }
+    // naive solution
+//    public void moveZeroes(int[] nums) {
+//        int num = 0;
+//        for(int i=0;i<nums.length-1;i++){
+//            if(nums[i]==0){
+//                for(int j=i;j<nums.length-1;j++){
+//                    int tmp = nums[j];
+//                    nums[j] = nums[j+1];
+//                    nums[j+1] = tmp;
+//                }
+//                i--;
+//            }
+//            num++;
+//            if(num==nums.length) break;
+//        }
+//    }
 }
