037b5cb98a32de05c94c74571ed15267e80703eb
chengyaoZhang <tc2002618@126.com>
2019-01-30 22:40:18 -0800

    LC98 modify

--- src/LC98_ValidateBinarySearchTree.java
+++ src/LC98_ValidateBinarySearchTree.java
@@ -5,33 +5,17 @@ public class LC98_ValidateBinarySearchTree {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, Integer low, Integer high){
         if(root==null) return true;
-        if(root.left==null && root.right==null) return true;
-        boolean isValidLeft = isValidBST(root.left);
-        boolean isValidRight = isValidBST(root.right);
-        if(isValidLeft && isValidRight){
-            boolean l;
-            boolean r;
-            TreeNode cur = root.left;
-            int max;
-            if(cur==null) l = true;
-            else{
-                while(cur.right!=null) cur = cur.right;
-                max = cur.val;
-                l = max<root.val;
-            }
-            cur = root.right;
-            int min;
-            if(cur==null) r = true;
-            else{
-                while(cur.left!=null) cur = cur.left;
-                min = cur.val;
-                r = min>root.val;
-            }
-            return l && r;
-        }else{
-            return false;
+
+        if((low==null || root.val>low) && (high==null || root.val<high)){
+            return isValidBST(root.left, (Integer)low, root.val) && isValidBST(root.right, root.val, (Integer)high);
         }
+        else return false;
     }
 }
