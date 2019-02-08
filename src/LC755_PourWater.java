056ac61ce8df35fbb7ed0a93bf84e17cca6c085a
chengyaoZhang <tc2002618@126.com>
2019-02-07 21:32:41 -0800

    LC755

---
+++ src/LC755_PourWater.java
@@ -0,0 +1,46 @@
+public class LC755_PourWater {
+    public int[] pourWater(int[] heights, int V, int K) {
+        while(V > 0){
+            int l = indexLeftMin(heights, K);
+            if(l != K){
+                heights[l]++;
+            } else {
+                int r = indexRightMin(heights, K);
+                if(r != K){
+                    heights[r]++;
+                } else {
+                    heights[K]++;
+                }
+            }
+            V--;
+        }
+
+        return heights;
+    }
+
+    private int indexLeftMin(int[] heights, int K){
+        int min = K;
+        for(int i = K - 1; i >= 0; i--){
+            if(heights[i] < heights[min]){
+                min = i;
+            } else if(heights[i] > heights[i+1]){
+                break;
+            }
+        }
+
+        return min;
+    }
+
+    private int indexRightMin(int[] heights, int K){
+        int min = K;
+        for(int i = K + 1; i < heights.length; i++){
+            if(heights[i] < heights[min]){
+                min = i;
+            } else if(heights[i] > heights[i-1]){
+                break;
+            }
+        }
+
+        return min;
+    }
+}
