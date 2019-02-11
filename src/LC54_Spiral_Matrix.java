04e33809138c9bbcdca13dfdbed06cab0302830a
chengyaoZhang <tc2002618@126.com>
2019-02-10 19:22:12 -0800

    LC54 modify

--- src/LC54_Spiral_Matrix.java
+++ src/LC54_Spiral_Matrix.java
@@ -43,75 +43,41 @@ public class LC54_Spiral_Matrix {
             spiralRec(matrix, row+1, column+1, rowSize-2, columnSize-2, res);
         }
     }
+
+    // iterative solution
 //    public List<Integer> spiralOrder(int[][] matrix) {
-//        List<Integer> res = new ArrayList<Integer>();
-//        if(matrix==null || matrix.length==0) return res;
-//        int m = matrix.length;
-//        int n = matrix[0].length;
-//        int[][] visited = new int[m][n];
-//        int i = 0;
-//        int j = 0;
-//        while(true){
-//            if((i==0 || visited[i-1][j]==1)
-//                    && (i==m-1 || visited[i+1][j]==1)
-//                    && (j==0 || visited[i][j-1]==1)
-//                    && (j==n-1 || visited[i][j+1]==1)){
-//                if(visited[i][j]==0) res.add(matrix[i][j]);
-//                break;
+//        List<Integer> res = new ArrayList<>();
+//        if(matrix == null || matrix.length == 0) return res;
+//
+//        int width = matrix[0].length;
+//        int height = matrix.length;
+//        int startR = 0;
+//        int startC = 0;
+//
+//        while(width > 0 && height > 0){
+//            for(int i = startC; i < startC + width; i++){
+//                res.add(matrix[startR][i]);
 //            }
-//            // at the upper left corner
-//            if((i==0 || visited[i-1][j]==1) && (j==0 || visited[i][j-1]==1) && j!=n-1 && visited[i][j+1]==0){
-//                if(visited[i][j]==0){
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//                // go right
-//                while(!(j==n-1 || visited[i][j+1]==1)){
-//                    j++;
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
+//            for(int i = startR + 1; i < startR + height; i++){
+//                res.add(matrix[i][startC + width - 1]);
+//            }
+//            if(height > 1){
+//                for(int i = startC + width - 2; i >= startC; i--){
+//                    res.add(matrix[startR + height - 1][i]);
 //                }
 //            }
-//            // at the upper right corner
-//            else if((i==0 || visited[i-1][j]==1) && (j==n-1 || visited[i][j+1]==1) && i!=m-1 && visited[i+1][j]==0){
-//                if(visited[i][j]==0){
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//                // go down
-//                while(!(i==m-1 || visited[i+1][j]==1)){
-//                    i++;
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
+//            if(width > 1){
+//                for(int i = startR + height - 2; i > startR; i--){
+//                    res.add(matrix[i][startC]);
 //                }
 //            }
-//            // at the lower right corner
-//            else if((i==m-1 || visited[i+1][j]==1) && (j==n-1 || visited[i][j+1]==1) && j!=0 && visited[i][j-1]==0){
-//                if(visited[i][j]==0){
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//                // go left
-//                while(!(j==0 || visited[i][j-1]==1)){
-//                    j--;
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//            }
-//            // at the lower left corner
-//            else{
-//                if(visited[i][j]==0){
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//                // go up
-//                while(!(i==0 || visited[i-1][j]==1)){
-//                    i--;
-//                    visited[i][j] = 1;
-//                    res.add(matrix[i][j]);
-//                }
-//            }
+//
+//            width -= 2;
+//            height -= 2;
+//            startR++;
+//            startC++;
 //        }
+//
 //        return res;
 //    }
 }
