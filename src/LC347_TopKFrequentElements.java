5cd8509ab7b98b03a7a9e40eda34cda25a6df174
chengyaoZhang <tc2002618@126.com>
2019-02-15 19:37:52 -0800

    LC347 modify

--- src/LC347_TopKFrequentElements.java
+++ src/LC347_TopKFrequentElements.java
@@ -1,21 +1,51 @@
 import java.util.*;
 
 public class LC347_TopKFrequentElements {
+    // O(n)
     public List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for(int num: nums){
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-        ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>(){
-            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2){
-                return e2.getValue().compareTo(e1.getValue());
+
+        ArrayList<Integer>[] bucket = new ArrayList[nums.length + 1];
+
+        for(int key: count.keySet()){
+            int val = count.get(key);
+            if(bucket[val] == null){
+                bucket[val] = new ArrayList<>();
             }
-        });
-        List<Integer> res = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            res.add(list.get(i).getKey());
+            bucket[val].add(key);
         }
+
+        List<Integer> res = new ArrayList<>();
+        for(int i = bucket.length - 1; i >= 0 && res.size() < k; i--){
+            if(bucket[i] == null) continue;
+
+            for(int num: bucket[i]){
+                res.add(num);
+                if(res.size() == k) break;
+            }
+        }
+
         return res;
     }
+
+//    public List<Integer> topKFrequent(int[] nums, int k) {
+//        HashMap<Integer, Integer> count = new HashMap<>();
+//        for(int num: nums){
+//            count.put(num, count.getOrDefault(num, 0) + 1);
+//        }
+//
+//        PriorityQueue<Integer> minHeap = new PriorityQueue<>((x, y) -> count.get(x) - count.get(y));
+//
+//        for(int key: count.keySet()){
+//            minHeap.offer(key);
+//            if(minHeap.size() > k){
+//                minHeap.poll();
+//            }
+//        }
+//
+//        return new ArrayList<>(minHeap);
+//    }
 }
