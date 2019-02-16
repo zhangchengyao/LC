df11d06c1e25d93ae6a9ebc1e6cb9c3c9e3b95e0
chengyaoZhang <tc2002618@126.com>
2019-02-15 16:49:34 -0800

    LC220 modify

--- src/LC220_ContainsDuplicateIII.java
+++ src/LC220_ContainsDuplicateIII.java
@@ -2,15 +2,19 @@ import java.util.TreeSet;
 
 public class LC220_ContainsDuplicateIII {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if(nums.length<2 || k<1) return false;
-        TreeSet<Long> set = new TreeSet<>();
-        for(int i=0;i<nums.length;i++){
-            Long ceil = set.ceiling((long)nums[i]);
-            Long floor = set.floor((long)nums[i]);
-            if((ceil!=null && ceil-nums[i]<=t) || (floor!=null && nums[i]-floor<=t)) return true;
-            set.add((long)nums[i]);
-            if(i-k>=0) set.remove((long)nums[i-k]);
+        if(nums.length < 2 || k < 1) return false;
+
+        TreeSet<Long> tset = new TreeSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            Long low = tset.floor((long)nums[i]);
+            Long high = tset.ceiling((long)nums[i]);
+            if((low != null && nums[i] - low <= t) || (high != null && high - nums[i] <= t)) return true;
+
+            if(i - k >= 0) tset.remove((long)nums[i - k]);
+            tset.add((long)nums[i]);
         }
+
         return false;
     }
 }
