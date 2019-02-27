4647fcb323ea98995e931ef709bb13fde9cb14b9
chengyaoZhang <tc2002618@126.com>
2019-02-27 09:08:39 -0800

    L285

---
+++ src/LC285_InorderSuccessorInBST.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class LC285_InorderSuccessorInBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null){
+            if(cur == p){
+                return getSucc(cur, stack);
+            } else if(cur.val < p.val){
+                stack.push(cur);
+                cur = cur.right;
+            } else {
+                stack.push(cur);
+                cur = cur.left;
+            }
+        }
+
+        return null;
+    }
+
+    private TreeNode getSucc(TreeNode root, Stack<TreeNode> stack){
+        if(root.right == null){
+            while(!stack.isEmpty()){
+                TreeNode node = stack.pop();
+                if(node.val > root.val){
+                    return node;
+                }
+            }
+            return null;
+        } else {
+            TreeNode cur = root.right;
+            while(cur.left != null){
+                cur = cur.left;
+            }
+            return cur;
+        }
+    }
+}
