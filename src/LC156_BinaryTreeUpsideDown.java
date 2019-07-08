c166d7856ebf6673002f60100aa1b0ce1e5e20b8
chengyaoZhang <tc2002618@126.com>
2019-07-07 22:17:37 -0700

    LC 156 modify

--- src/LC156_BinaryTreeUpsideDown.java
+++ src/LC156_BinaryTreeUpsideDown.java
@@ -7,23 +7,34 @@ public class LC156_BinaryTreeUpsideDown {
     }
 
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-        if(root == null) return root;
-        TreeNode cur = root;
-        while(cur.left != null){
-            cur = cur.left;
-        }
-        return flip(cur, root);
+        if(root == null || root.left == null) return root;
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
     }
 
-    private TreeNode flip(TreeNode cur, TreeNode root) {
-        if(cur == root || cur == null) return cur;
-
-        TreeNode parent = root;
-        while(parent.left != cur) parent = parent.left;
-        cur.left = parent.right;
-        parent.right = null;
-        parent.left = null;
-        cur.right = flip(parent, root);
-        return cur;
-    }
+//    public TreeNode upsideDownBinaryTree(TreeNode root) {
+//        if(root == null) return root;
+//        TreeNode cur = root;
+//        while(cur.left != null){
+//            cur = cur.left;
+//        }
+//        return flip(cur, root);
+//    }
+//
+//    private TreeNode flip(TreeNode cur, TreeNode root) {
+//        if(cur == root || cur == null) return cur;
+//
+//        TreeNode parent = root;
+//        while(parent.left != cur) parent = parent.left;
+//        cur.left = parent.right;
+//        parent.right = null;
+//        parent.left = null;
+//        cur.right = flip(parent, root);
+//        return cur;
+//    }
 }
