88343b4b2f8d5920f404c8d1ecba0bf6cd276a3f
chengyaoZhang <tc2002618@126.com>
2019-07-07 21:58:14 -0700

    LC 156

---
+++ src/LC156_BinaryTreeUpsideDown.java
@@ -0,0 +1,29 @@
+public class LC156_BinaryTreeUpsideDown {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null) return root;
+        TreeNode cur = root;
+        while(cur.left != null){
+            cur = cur.left;
+        }
+        return flip(cur, root);
+    }
+
+    private TreeNode flip(TreeNode cur, TreeNode root) {
+        if(cur == root || cur == null) return cur;
+
+        TreeNode parent = root;
+        while(parent.left != cur) parent = parent.left;
+        cur.left = parent.right;
+        parent.right = null;
+        parent.left = null;
+        cur.right = flip(parent, root);
+        return cur;
+    }
+}
