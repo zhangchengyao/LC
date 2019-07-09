10bb46189d8062f2948d182ad84f7b7256e44199
chengyaoZhang <tc2002618@126.com>
2019-07-08 21:15:12 -0700

    LC 158 modify

--- src/LC158_ReadNCharactersGivenRead4IICallMultipleTimes.java
+++ src/LC158_ReadNCharactersGivenRead4IICallMultipleTimes.java
@@ -4,20 +4,23 @@ public class LC158_ReadNCharactersGivenRead4IICallMultipleTimes {
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
-    int writePos = 0;
-    int readPos = 0;
-    char[] buff = new char[4];
+    private int read = 0;
+    private int count = 0;
+    private char[] buff = new char[4];
+
     public int read(char[] buf, int n) {
-        for(int i=0;i<n;i++){
-            if(readPos==writePos){
-                writePos = read4(buff);
-                readPos = 0;
-                if(writePos==0) return i;
+        int total = 0;
+        for(int i = 0; i < n; i++) {
+            if(read == count) {
+                count = read4(buff);
+                read = 0;
             }
-            buf[i] = buff[readPos];
-            readPos++;
+            if(count == 0) break;
+            buf[i] = buff[read++];
+            total++;
         }
-        return n;
+
+        return total;
     }
     int read4(char[] buff){
         return 4;
