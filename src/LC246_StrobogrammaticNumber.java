15d50a4e9d2a4a54cbcf538079cef2dda9fc39fa
chengyaoZhang <tc2002618@126.com>
2019-07-14 11:58:24 -0700

    LC 246 modify

--- src/LC246_StrobogrammaticNumber.java
+++ src/LC246_StrobogrammaticNumber.java
@@ -2,19 +2,18 @@ public class LC246_StrobogrammaticNumber {
     public boolean isStrobogrammatic(String num) {
         int i = 0;
         int j = num.length()-1;
-        while(i<=j){
-            if(i==j) return num.charAt(i)=='0' || num.charAt(i)=='1' || num.charAt(i)=='8';
-            if(num.charAt(i)=='0'){
-                if(num.charAt(j)!='0') return false;
-            }else if(num.charAt(i)=='1'){
-                if(num.charAt(j)!='1') return false;
-            }else if(num.charAt(i)=='8'){
-                if(num.charAt(j)!='8') return false;
-            }else if(num.charAt(i)=='6'){
-                if(num.charAt(j)!='9') return false;
-            }else if(num.charAt(i)=='9'){
-                if(num.charAt(j)!='6') return false;
-            }else return false;
+        while(i <= j){
+            boolean is018 = num.charAt(i) == '0' || num.charAt(i) == '1' || num.charAt(i) == '8';
+            if(i == j) return is018;
+            if(is018) {
+                if(num.charAt(i) != num.charAt(j)) return false;
+            } else if(num.charAt(i) == '6') {
+                if(num.charAt(j) != '9') return false;
+            } else if(num.charAt(i) == '9') {
+                if(num.charAt(j) != '6') return false;
+            } else {
+                return false;
+            }
             i++;
             j--;
         }
