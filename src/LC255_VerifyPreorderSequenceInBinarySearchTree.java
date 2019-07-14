a8ef5cad0a59b9bcfb1a5516ca267052078e2a8d
chengyaoZhang <tc2002618@126.com>
2019-07-14 11:00:32 -0700

    LC 255 modify

--- src/LC255_VerifyPreorderSequenceInBinarySearchTree.java
+++ src/LC255_VerifyPreorderSequenceInBinarySearchTree.java
@@ -2,20 +2,37 @@ public class LC255_VerifyPreorderSequenceInBinarySearchTree {
     public boolean verifyPreorder(int[] preorder) {
         if(preorder == null || preorder.length == 0) return true;
 
-        return verify(preorder, 0, preorder.length - 1, null, null);
-    }
-
-    private boolean verify(int[] preorder, int left, int right, Integer lower, Integer upper) {
-        if(left >= right) return true;
-
-        int root = preorder[left];
-
-        int split = left + 1;
-        while(split <= right && preorder[split] < root) {
-            if((lower != null && preorder[split] < lower) || (upper != null && preorder[split] > upper)) return false;
-            split++;
+        int low = Integer.MIN_VALUE;
+        int p = -1;
+        for(int node: preorder) {
+            if(node < low) return false;
+            while(p >= 0 && preorder[p] < node) {
+                low = preorder[p];
+                p--;
+            }
+            preorder[++p] = node;
         }
 
-        return verify(preorder, left + 1, split - 1, lower, root) && verify(preorder, split, right, root, upper);
+        return true;
     }
+
+//    public boolean verifyPreorder(int[] preorder) {
+//        if(preorder == null || preorder.length == 0) return true;
+//
+//        return verify(preorder, 0, preorder.length - 1, null, null);
+//    }
+//
+//    private boolean verify(int[] preorder, int left, int right, Integer lower, Integer upper) {
+//        if(left >= right) return true;
+//
+//        int root = preorder[left];
+//
+//        int split = left + 1;
+//        while(split <= right && preorder[split] < root) {
+//            if((lower != null && preorder[split] < lower) || (upper != null && preorder[split] > upper)) return false;
+//            split++;
+//        }
+//
+//        return verify(preorder, left + 1, split - 1, lower, root) && verify(preorder, split, right, root, upper);
+//    }
 }
