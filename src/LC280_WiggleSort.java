567b1e43a015d7719aa81de64c4e9933a6f834af
chengyaoZhang <tc2002618@126.com>
2019-07-20 11:38:39 -0700

    LC 280 modify

--- src/LC280_WiggleSort.java
+++ src/LC280_WiggleSort.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class LC280_WiggleSort {
     public void wiggleSort(int[] nums) {
-        if(nums==null || nums.length<2) return;
+        if(nums == null || nums.length < 2) return;
         int i = 0;
         int j = 1;
-        while(j<nums.length){
-            if(((j&1)==1 && nums[j]<nums[i]) || ((j&1)==0 && nums[j]>nums[i])) swap(nums, i, j);
+        while(j < nums.length){
+            if(((j&1) == 1 && nums[j] < nums[i]) || ((j&1) == 0 && nums[j] > nums[i])) swap(nums, i, j);
             i++;
             j++;
         }
