1e0defd7e004fdbf56dc686f03a0055e046d4edb
chengyaoZhang <tc2002618@126.com>
2019-07-20 13:45:24 -0700

    LC 286 modify

--- src/LC286_WallsAndGates.java
+++ src/LC286_WallsAndGates.java
@@ -1,9 +1,9 @@
 public class LC286_WallsAndGates {
     public void wallsAndGates(int[][] rooms) {
-        if(rooms==null || rooms.length==0) return;
-        for(int i=0;i<rooms.length;i++){
-            for(int j=0;j<rooms[0].length;j++){
-                if(rooms[i][j]==0){
+        if(rooms == null || rooms.length == 0) return;
+        for(int i = 0; i < rooms.length; i++){
+            for(int j = 0; j < rooms[0].length; j++){
+                if(rooms[i][j] == 0){
                     probe(rooms, i-1, j, 1);
                     probe(rooms, i+1, j, 1);
                     probe(rooms, i, j-1, 1);
@@ -12,14 +12,14 @@ public class LC286_WallsAndGates {
             }
         }
     }
-    private void probe(int[][] rooms, int x, int y, int dis){
-        if(x<0 || x>=rooms.length || y<0 || y>=rooms[0].length) return;
-        if(rooms[x][y]==-1 || rooms[x][y]==0) return;
-        if(rooms[x][y]<=dis) return;
-        else rooms[x][y] = dis;
-        probe(rooms, x-1, y, dis+1);
-        probe(rooms, x+1, y, dis+1);
-        probe(rooms, x, y-1, dis+1);
-        probe(rooms, x, y+1, dis+1);
+    private void probe(int[][] rooms, int x, int y, int dist){
+        if(x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length) return;
+        if(rooms[x][y] == -1 || rooms[x][y] == 0) return;
+        if(rooms[x][y] <= dist) return;
+        else rooms[x][y] = dist;
+        probe(rooms, x-1, y, dist+1);
+        probe(rooms, x+1, y, dist+1);
+        probe(rooms, x, y-1, dist+1);
+        probe(rooms, x, y+1, dist+1);
     }
 }
