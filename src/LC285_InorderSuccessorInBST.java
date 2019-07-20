2553e1afbbd318d458d7f2c90266d149bbef949d
chengyaoZhang <tc2002618@126.com>
2019-07-20 12:24:33 -0700

    LC 285 modify

--- src/LC285_InorderSuccessorInBST.java
+++ src/LC285_InorderSuccessorInBST.java
@@ -8,6 +8,36 @@ public class LC285_InorderSuccessorInBST {
         TreeNode(int x) { val = x; }
     }
 
+//    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+//        TreeNode prev = null;
+//        TreeNode cur = root;
+//        boolean found = false;
+//
+//        while(cur != null) {
+//            if(cur.left != null) {
+//                prev = cur.left;
+//                while(prev.right != null && prev.right != cur) prev = prev.right;
+//                if(prev.right == null) {
+//                    prev.right = cur;
+//                    cur = cur.left;
+//                } else {
+//                    prev.right = null;
+//                    if(found) return cur;
+//                    if(cur == p) found = true;
+//                    prev = cur;
+//                    cur = cur.right;
+//                }
+//            } else {
+//                if(found) return cur;
+//                if(cur == p) found = true;
+//                prev = cur;
+//                cur = cur.right;
+//            }
+//        }
+//
+//        return null;
+//    }
+
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode cur = root;
