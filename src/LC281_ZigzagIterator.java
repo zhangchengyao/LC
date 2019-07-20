a2125e17791b20694b3406d684da6aa5c5466f3b
chengyaoZhang <tc2002618@126.com>
2019-07-20 12:10:09 -0700

    LC 281 modify

--- src/LC281_ZigzagIterator.java
+++ src/LC281_ZigzagIterator.java
@@ -7,12 +7,12 @@ public class LC281_ZigzagIterator {
     public LC281_ZigzagIterator(List<Integer> v1, List<Integer> v2) {
         list = new ArrayList<>();
         int i = 0;
-        while(i<v1.size() && i<v2.size()){
+        while(i < v1.size() && i < v2.size()){
             list.add(v1.get(i));
             list.add(v2.get(i++));
         }
-        while(i<v1.size()) list.add(v1.get(i++));
-        while(i<v2.size()) list.add(v2.get(i++));
+        while(i < v1.size()) list.add(v1.get(i++));
+        while(i < v2.size()) list.add(v2.get(i++));
         cur = 0;
     }
 
@@ -21,6 +21,6 @@ public class LC281_ZigzagIterator {
     }
 
     public boolean hasNext() {
-        return cur!=list.size();
+        return cur != list.size();
     }
 }
