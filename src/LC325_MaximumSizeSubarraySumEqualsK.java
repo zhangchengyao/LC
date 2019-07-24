f0ceff93f7aa88b07e5f749344e82fc5a15785ea
chengyaoZhang <tc2002618@126.com>
2019-07-23 19:40:30 -0700

    LC 325 modify

--- src/LC325_MaximumSizeSubarraySumEqualsK.java
+++ src/LC325_MaximumSizeSubarraySumEqualsK.java
@@ -6,29 +6,17 @@ public class LC325_MaximumSizeSubarraySumEqualsK {
     public int maxSubArrayLen(int[] nums, int k) {
         int longest = 0;
 
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
         int sum = 0;
         for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            map.putIfAbsent(sum, new ArrayList<>());
-            map.get(sum).add(i);
+            if(map.containsKey(sum - k)) {
+                longest = Math.max(longest, i - map.get(sum - k));
+            }
+            map.putIfAbsent(sum, i);
         }
 
-        for(int key: map.keySet()) {
-            if(key == k) {
-                List<Integer> list = map.get(key);
-                longest = Math.max(longest, list.get(list.size() - 1) + 1);
-            } else {
-                int diff = key - k;
-                if(map.containsKey(diff)){
-                    List<Integer> list1 = map.get(diff);
-                    List<Integer> list2 = map.get(key);
-                    if(list1.get(0) < list2.get(list2.size() - 1)) {
-                        longest = Math.max(longest, list2.get(list2.size() - 1) - list1.get(0));
-                    }
-                }
-            }
-        }
 
         return longest;
     }
