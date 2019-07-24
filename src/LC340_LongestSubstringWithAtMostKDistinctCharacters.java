a7121e789bde0af0e28da0b02c5a9d1d6d463bc5
chengyaoZhang <tc2002618@126.com>
2019-07-23 20:47:17 -0700

    LC 340 modify

--- src/LC340_LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/LC340_LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,22 +1,21 @@
 public class LC340_LongestSubstringWithAtMostKDistinctCharacters {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if(s.length()==0||k==0) return 0;
+        if(s.length() == 0 || k==0) return 0;
+
         int res = 0;
         int left = 0;
         int right = 0;
         int[] map = new int[256];
         int cnt = 0;
         while(right<s.length()){
-            if(map[s.charAt(right)]==0) cnt++;
+            if(map[s.charAt(right)] == 0) cnt++;
             map[s.charAt(right)]++;
-            if(cnt>k){
-                while(cnt>k){
-                    map[s.charAt(left)]--;
-                    if(map[s.charAt(left)]==0) cnt--;
-                    left++;
-                }
+            while(cnt > k){
+                map[s.charAt(left)]--;
+                if(map[s.charAt(left)]==0) cnt--;
+                left++;
             }
-            res = Math.max(res, right-left+1);
+            res = Math.max(res, right - left + 1);
             right++;
         }
         return res;
