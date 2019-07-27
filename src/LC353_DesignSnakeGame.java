5bece6e5206aa87537d536715605602e46fbccad
chengyaoZhang <tc2002618@126.com>
2019-07-27 11:52:00 -0700

    LC 353 modify

--- src/LC353_DesignSnakeGame.java
+++ src/LC353_DesignSnakeGame.java
@@ -1,24 +1,30 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LC353_DesignSnakeGame {
-    List<int[]> snake;
-    int[][] food;
-    int score;
-    int width;
-    int height;
+    private int width;
+    private int height;
+    private int[][] food;
+    private LinkedList<int[]> snake;
+    private HashSet<Integer> body;
+    private int score;
+
     /** Initialize your data structure here.
      @param width - screen width
      @param height - screen height
      @param food - A list of food positions
      E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
     public LC353_DesignSnakeGame(int width, int height, int[][] food) {
-        snake = new ArrayList<>();
-        this.food = food;
-        score = 0;
         this.width = width;
         this.height = height;
-        snake.add(new int[]{0, 0});
+        this.food = food;
+        snake = new LinkedList<>();
+        snake.offer(new int[]{0, 0});
+        body = new HashSet<>();
+        body.add(0);
+        score = 0;
     }
 
     /** Moves the snake.
@@ -26,23 +32,39 @@ public class LC353_DesignSnakeGame {
      @return The game's score after the move. Return -1 if game over.
      Game over when snake crosses the screen boundary or bites its body. */
     public int move(String direction) {
-        int[] head = snake.get(0);
-        int x = head[0];
-        int y = head[1];
-        int[] tail = snake.remove(snake.size()-1);
-        if(direction.equals("U")) x--;
-        else if(direction.equals("L")) y--;
-        else if(direction.equals("R")) y++;
-        else x++;
-        if(x<0 || y<0 || x>=height || y>=width) return -1;
-        for(int[] body: snake){
-            if(x==body[0] && y==body[1]) return -1;
+        int[] tail = snake.peek();
+        int[] head = snake.peekLast();
+        int r, c;
+        switch (direction) {
+            case "U":
+                r = head[0] - 1;
+                c = head[1];
+                break;
+            case "L":
+                r = head[0];
+                c = head[1] - 1;
+                break;
+            case "R":
+                r = head[0];
+                c = head[1] + 1;
+                break;
+            default:
+                r = head[0] + 1;
+                c = head[1];
+                break;
         }
-        if(score<food.length && x==food[score][0] && y==food[score][1]){
-            snake.add(tail);
+
+        if(r < 0 || r >= height || c < 0 || c >= width || (body.contains(r * width + c) && !(tail[0] == r && tail[1] == c))) return -1;
+
+        if(score < food.length && food[score][0] == r && food[score][1] == c) {
             score++;
+        } else {
+            body.remove(tail[0] * width + tail[1]);
+            snake.poll();
         }
-        snake.add(0, new int[]{x, y});
+        snake.offer(new int[]{r, c});
+        body.add(r * width + c);
+
         return score;
     }
 }
