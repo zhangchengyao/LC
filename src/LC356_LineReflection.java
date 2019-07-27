d70c8852be24dfa4cea91f171785606acbb41852
chengyaoZhang <tc2002618@126.com>
2019-07-27 12:23:42 -0700

    LC 356 modify

--- src/LC356_LineReflection.java
+++ src/LC356_LineReflection.java
@@ -3,35 +3,30 @@ import java.util.TreeSet;
 
 public class LC356_LineReflection {
     public boolean isReflected(int[][] points) {
-        HashMap<Integer, TreeSet<Integer>> map = new HashMap<>();
-        for (int[] point : points) {
-            TreeSet<Integer> tset = map.getOrDefault(point[1], new TreeSet<>());
-            tset.add(point[0]);
-            map.put(point[1], tset);
+        if(points == null) return false;
+
+        HashMap<Integer, TreeSet<Integer>> group = new HashMap<>();
+        for(int[] point: points) {
+            int y = point[1];
+            group.putIfAbsent(y, new TreeSet<>());
+            group.get(y).add(point[0]);
         }
-        double mid = 0;
-        boolean first = true;
-        for (TreeSet<Integer> xs : map.values()) {
-            while (!xs.isEmpty()) {
-                if (xs.size() == 1) {
-                    if (first) {
-                        first = false;
-                        mid = xs.pollFirst();
-                    } else {
-                        if (mid != xs.pollFirst()) return false;
-                    }
+
+        Double line = null;
+        for(TreeSet<Integer> xs: group.values()) {
+            while(!xs.isEmpty()) {
+                int a = xs.first();
+                int b = xs.last();
+                if(line == null) {
+                    line = (a + b) / 2.0;
                 } else {
-                    int a = xs.pollFirst();
-                    int b = xs.pollLast();
-                    if (first) {
-                        first = false;
-                        mid = (a + b) / 2.0;
-                    } else {
-                        if (mid != (a + b) / 2.0) return false;
-                    }
+                    if((a + b) / 2.0 != line) return false;
+                    xs.pollFirst();
+                    if(a != b) xs.pollLast();
                 }
             }
         }
+
         return true;
     }
 }
