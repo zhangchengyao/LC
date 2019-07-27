49d66c99536b3a8e99c9fa19ab757869427d7353
chengyaoZhang <tc2002618@126.com>
2019-07-27 16:23:25 -0700

    LC 370 modify

--- src/LC370_RangeAddition.java
+++ src/LC370_RangeAddition.java
@@ -2,7 +2,7 @@ public class LC370_RangeAddition {
     public int[] getModifiedArray(int length, int[][] updates) {
         int[] res = new int[length];
         for(int[] update: updates){
-            for(int i=update[0];i<=update[1];i++){
+            for(int i = update[0]; i <= update[1]; i++){
                 res[i] += update[2];
             }
         }
@@ -13,9 +13,9 @@ public class LC370_RangeAddition {
 //        int[] res = new int[length];
 //        for(int[] update: updates){
 //            res[update[0]] += update[2];
-//            if(update[1]<length-1) res[update[1]+1] -= update[2];
+//            if(update[1] < length - 1) res[update[1]+1] -= update[2];
 //        }
-//        for(int i=1;i<length;i++){
+//        for(int i = 1; i < length; i++){
 //            res[i] += res[i-1];
 //        }
 //        return res;
