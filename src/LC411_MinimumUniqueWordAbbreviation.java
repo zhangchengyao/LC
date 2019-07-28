03c12d31a8dfcfb6abab3062b836eaaa02882c26
chengyaoZhang <tc2002618@126.com>
2019-07-28 11:00:59 -0700

    LC 411 modify

--- src/LC411_MinimumUniqueWordAbbreviation.java
+++ src/LC411_MinimumUniqueWordAbbreviation.java
@@ -3,12 +3,12 @@ public class LC411_MinimumUniqueWordAbbreviation {
         int m = target.length();
         int minLen = Integer.MAX_VALUE;
         String res = "";
-        for(int i=1;i<=(1<<m);i++){
+        for(int i = 1; i <= (1 << m); i++){
             int len = abbrLen(target, i);
-            if(len>=minLen) continue;
+            if(len >= minLen) continue;
             boolean conflict = false;
             for(String word: dictionary){
-                if(word.length()!=m) continue;
+                if(word.length() != m) continue;
                 if(hasConflict(target, word, i)){
                     conflict = true;
                     break;
@@ -24,19 +24,19 @@ public class LC411_MinimumUniqueWordAbbreviation {
     private int abbrLen(String target, int bVal){
         int m = target.length();
         int len = 0;
-        for(int i=0;i<m;i++){
-            if((bVal>>i & 1)==1) len++;
+        for(int i = 0; i < m; i++){
+            if((bVal >> i & 1) == 1) len++;
             else{
-                if((bVal>>(i+1) & 1)==1 || i==m-1) len++;
+                if((bVal >> (i+1) & 1) == 1 || i == m-1) len++;
             }
         }
         return len;
     }
     private boolean hasConflict(String target, String word, int bVal){
         int m = target.length();
-        for(int i=0;i<m;i++){
-            if((bVal>>i & 1)==1){
-                if(target.charAt(m-1-i)!=word.charAt(m-1-i)) return false;
+        for(int i = 0; i < m; i++){
+            if((bVal >> i & 1) == 1){
+                if(target.charAt(m - 1 - i) != word.charAt(m - 1 - i)) return false;
             }
         }
         return true;
@@ -45,17 +45,17 @@ public class LC411_MinimumUniqueWordAbbreviation {
         StringBuilder sb = new StringBuilder();
         int m = target.length();
         int numZero = 0;
-        for(int i=0;i<m;i++){
-            if((bVal>>i & 1)==1){
-                if(numZero!=0) sb.insert(0, numZero);
-                sb.insert(0, target.charAt(m-1-i));
+        for(int i = 0; i < m; i++){
+            if((bVal >> i & 1) == 1){
+                if(numZero != 0) sb.insert(0, numZero);
+                sb.insert(0, target.charAt(m - 1 - i));
                 numZero = 0;
             }
             else{
                 numZero++;
             }
         }
-        if(numZero!=0) sb.insert(0, numZero);
+        if(numZero != 0) sb.insert(0, numZero);
         return sb.toString();
     }
 }
