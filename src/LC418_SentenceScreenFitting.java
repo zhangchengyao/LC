321dcaf00ae5ca826f689d3e3d3decac38b91200
chengyaoZhang <tc2002618@126.com>
2019-07-28 11:38:24 -0700

    LC 418 modify

--- src/LC418_SentenceScreenFitting.java
+++ src/LC418_SentenceScreenFitting.java
@@ -1,22 +1,24 @@
 public class LC418_SentenceScreenFitting {
     public int wordsTyping(String[] sentence, int rows, int cols) {
-        if(rows==0 || cols==0) return 0;
+        if(rows == 0 || cols == 0) return 0;
+
         int totalLen = 0;
-        for(String s: sentence) totalLen += s.length()+1;
-        int colRemain = cols;
+        for(String s: sentence) totalLen += s.length() + 1;
+
+        int colRemain;
         int idx = 0;
         int res = 0;
-        for(int r=0;r<rows;r++){
+        for(int r = 0; r < rows; r++){
             colRemain = cols;
-            while(colRemain>0){
-                if(colRemain>=sentence[idx].length()){
+            while(colRemain > 0){
+                if(colRemain >= sentence[idx].length()){
                     colRemain -= sentence[idx].length();
-                    if(colRemain>0) colRemain--;
+                    if(colRemain > 0) colRemain--;
                     idx++;
                 }
                 else break;
-                if(idx>=sentence.length){
-                    res += 1 + colRemain/totalLen;
+                if(idx == sentence.length){
+                    res += 1 + colRemain / totalLen;
                     colRemain %= totalLen;
                     idx = 0;
                 }
