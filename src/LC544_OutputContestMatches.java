8e34246ce1663000a5d2fc94aa91effa4499ddaf
chengyaoZhang <tc2002618@126.com>
2019-07-28 22:50:17 -0700

    LC 544 modify

--- src/LC544_OutputContestMatches.java
+++ src/LC544_OutputContestMatches.java
@@ -4,15 +4,31 @@ import java.util.List;
 public class LC544_OutputContestMatches {
     public String findContestMatch(int n) {
         List<String> teams = new ArrayList<>();
-        for(int i=1;i<=n;i++) teams.add(i+"");
-        return match(teams);
-    }
-    private String match(List<String> teams){
-        if(teams.size()==2) return "("+teams.get(0)+","+teams.get(1)+")";
-        List<String> newTeams = new ArrayList<>();
-        for(int i=0;i<teams.size()/2;i++){
-            newTeams.add("("+teams.get(i)+","+teams.get(teams.size()-i-1)+")");
+        for(int i = 1; i <= n; i++) {
+            teams.add(Integer.toString(i));
         }
-        return match(newTeams);
+        while(teams.size() > 1) {
+            List<String> next = new ArrayList<>();
+            for(int i = 0; i < teams.size() / 2; i++) {
+                next.add("(" + teams.get(i) + "," + teams.get(teams.size() - 1 - i) + ")");
+            }
+            teams = next;
+        }
+
+        return teams.get(0);
     }
+
+//    public String findContestMatch(int n) {
+//        List<String> teams = new ArrayList<>();
+//        for(int i = 1; i <= n; i++) teams.add(i + "");
+//        return match(teams);
+//    }
+//    private String match(List<String> teams){
+//        if(teams.size() == 2) return "(" + teams.get(0) + "," + teams.get(1) + ")";
+//        List<String> newTeams = new ArrayList<>();
+//        for(int i = 0; i < teams.size() / 2; i++){
+//            newTeams.add("(" + teams.get(i) + "," + teams.get(teams.size() - i - 1) + ")");
+//        }
+//        return match(newTeams);
+//    }
 }
