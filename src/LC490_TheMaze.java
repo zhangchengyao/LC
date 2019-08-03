3ab0ff035235e31d88c8e763b1fb849bdd75aad8
chengyaoZhang <tc2002618@126.com>
2019-08-02 19:36:26 -0700

    LC 490 modify

--- src/LC490_TheMaze.java
+++ src/LC490_TheMaze.java
@@ -4,63 +4,47 @@ import java.util.Queue;
 
 public class LC490_TheMaze {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        HashSet<String> seen = new HashSet<>();
-        Queue<String> q = new LinkedList<>();
-        String cur = start[0]+" "+start[1];
-        q.offer(cur);
-        seen.add(cur);
-        while(!q.isEmpty()){
-            cur = q.poll();
-            int[] coord = getCoord(cur);
-            if(coord[0]==destination[0]&&coord[1]==destination[1]) return true;
-            if(coord[0]>0 && maze[coord[0]-1][coord[1]]==0){
-                String next = getNextState(maze, coord[0], coord[1], 'U');
-                if(!seen.contains(next)){
-                    seen.add(next);
-                    q.offer(next);
-                }
-            }
-            if(coord[0]<maze.length-1 && maze[coord[0]+1][coord[1]]==0){
-                String next = getNextState(maze, coord[0], coord[1], 'D');
-                if(!seen.contains(next)){
-                    seen.add(next);
-                    q.offer(next);
-                }
-            }
-            if(coord[1]<maze[0].length-1 && maze[coord[0]][coord[1]+1]==0){
-                String next = getNextState(maze, coord[0], coord[1], 'R');
-                if(!seen.contains(next)){
-                    seen.add(next);
-                    q.offer(next);
-                }
-            }
-            if(coord[1]>0 && maze[coord[0]][coord[1]-1]==0){
-                String next = getNextState(maze, coord[0], coord[1], 'L');
-                if(!seen.contains(next)){
-                    seen.add(next);
-                    q.offer(next);
+        int m = maze.length;
+        int n = maze[0].length;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        visited.add(start[0] * n + start[1]);
+        q.offer(start[0] * n + start[1]);
+
+        while(!q.isEmpty()) {
+            int pos = q.poll();
+            int curR = pos / n;
+            int curC = pos % n;
+            if(curR == destination[0] && curC == destination[1]) return true;
+            for(int[] dir: directions) {
+                int nextPos = getNextPos(maze, curR, curC, dir);
+                if(nextPos != pos && !visited.contains(nextPos)) {
+                    q.offer(nextPos);
+                    visited.add(nextPos);
                 }
             }
         }
+
         return false;
     }
-    private int[] getCoord(String s){
-        String[] str = s.split(" ");
-        return new int[]{Integer.parseInt(str[0]), Integer.parseInt(str[1])};
-    }
-    private String getNextState(int[][] maze, int x, int y, char direct){
-        if(direct=='U'){
-            while(x>=0 && maze[x][y]==0) x--;
-            return (x+1)+" "+y;
-        }else if(direct=='D'){
-            while(x<maze.length && maze[x][y]==0) x++;
-            return (x-1)+" "+y;
-        }else if(direct=='R'){
-            while(y<maze[0].length && maze[x][y]==0) y++;
-            return x+" "+(y-1);
-        }else{
-            while(y>=0 && maze[x][y]==0) y--;
-            return x+" "+(y+1);
+
+    private int getNextPos(int[][] maze, int curR, int curC, int[] dir) {
+        int m = maze.length;
+        int n = maze[0].length;
+        while(valid(m, n, curR, curC) && maze[curR][curC] == 0) {
+            curR += dir[0];
+            curC += dir[1];
         }
+        curR -= dir[0];
+        curC -= dir[1];
+
+        return curR * n + curC;
+    }
+
+    private boolean valid(int m, int n, int curR, int curC) {
+        return curR >= 0 && curR < m && curC >= 0 && curC < n;
     }
 }
