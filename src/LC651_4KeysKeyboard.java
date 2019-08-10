04669522fdf00633d9321a3e639f8def8db9f7ad
chengyaoZhang <tc2002618@126.com>
2019-08-10 11:06:05 -0700

    LC 651

---
+++ src/LC651_4KeysKeyboard.java
@@ -0,0 +1,30 @@
+public class LC651_4KeysKeyboard {
+    public int maxA(int N) {
+        if(N < 7) return N;
+
+        int[] dp = new int[N + 1];
+        for(int i = 1; i < 7; i++) {
+            dp[i] = i;
+        }
+
+        for(int i = 7; i <= N; i++) {
+            for(int j = i - 3; j > 1; j--) {
+                dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+            }
+        }
+
+        return dp[N];
+    }
+
+//    public int maxA(int N) {
+//        if(N < 7) return N;
+//
+//        int max = 0;
+//        for(int i = N - 3; i > 1; i--) {
+//            int cur = maxA(i) * (N - i - 1);
+//            max = Math.max(max, cur);
+//        }
+//
+//        return max;
+//    }
+}
