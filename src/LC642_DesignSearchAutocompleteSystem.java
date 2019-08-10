f9bedf26b5b0a4b794b4d1b00f580a2ef86d8301
chengyaoZhang <tc2002618@126.com>
2019-08-10 10:44:01 -0700

    LC 642 modify

--- src/LC642_DesignSearchAutocompleteSystem.java
+++ src/LC642_DesignSearchAutocompleteSystem.java
@@ -1,120 +1,77 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LC642_DesignSearchAutocompleteSystem {
-    class Trie{
-        Node root;
-
-        Trie(){
-            root = new Node();
-        }
-
-        void insert(String sentence, int times){
-            Node cur = root;
-            for(int i = 0; i < sentence.length(); i++){
-                char c = sentence.charAt(i);
-                if(cur.children[c] == null){
-                    cur.children[c] = new Node(c);
-                }
-                cur = cur.children[c];
-            }
-            if(cur.sentence != null) cur.hot += times;
-            else{
-                cur.sentence = sentence;
-                cur.hot = times;
-            }
-        }
-
-        List<String> startsWith(String str){
-            List<Sentence> list = new ArrayList<>();
-            List<String> res = new ArrayList<>();
-
-            Node cur = root;
-            for(int i = 0; i < str.length(); i++){
-                char c = str.charAt(i);
-                if(cur.children[c] == null){
-                    return res;
-                }
-                cur = cur.children[c];
-            }
-
-            if(cur.sentence != null) list.add(new Sentence(cur.sentence, cur.hot));
-
-            for(Node child: cur.children){
-                if(child != null) dfs(child, list);
-            }
-
-            for(Sentence sentence: list) res.add(sentence.s);
-
-            return res;
-        }
-
-        private void dfs(Node cur, List<Sentence> res){
-            if(cur == null) return;
-
-            if(cur.sentence != null){
-                int i = 0;
-                for(; i < res.size(); i++){
-                    if(cur.hot > res.get(i).hot ||
-                            (cur.hot == res.get(i).hot && cur.sentence.compareTo(res.get(i).s) < 0)){
-                        res.add(i, new Sentence(cur.sentence, cur.hot));
-                        break;
-                    }
-                }
-
-                if(i == res.size() && res.size() < 3) res.add(new Sentence(cur.sentence, cur.hot));
-                if(res.size() > 3) res.remove(res.size() - 1);
-            }
-
-            for(Node child: cur.children){
-                if(child != null) dfs(child, res);
-            }
-        }
-    }
-    class Node{
-        char val;
+    static class Node {
         String sentence;
         int hot;
-        Node[] children;
-
-        Node(){
-            children = new Node[130];
-        }
-        Node(char c){
-            val = c;
-            children = new Node[130];
-        }
-    }
-    class Sentence{
-        String s;
-        int hot;
-        Sentence(String s, int hot){
-            this.s = s;
-            this.hot = hot;
+        HashMap<Character, Node> children;
+        Node() {
+            children = new HashMap<>();
         }
     }
 
-    private Trie trie;
-    private StringBuilder input;
+    private Node root;
+    private Node cur;
+    private String sentence;
 
     public LC642_DesignSearchAutocompleteSystem(String[] sentences, int[] times) {
-        trie = new Trie();
-        input = new StringBuilder();
-        for(int i = 0; i < sentences.length; i++){
-            trie.insert(sentences[i], times[i]);
+        root = new Node();
+        cur = root;
+        sentence = "";
+        for(int i = 0; i < sentences.length; i++) {
+            String s = sentences[i];
+            Node node = root;
+            for(char c: s.toCharArray()) {
+                if(!node.children.containsKey(c)) {
+                    node.children.put(c, new Node());
+                }
+                node = node.children.get(c);
+            }
+            node.sentence = s;
+            node.hot = times[i];
         }
     }
 
     public List<String> input(char c) {
-        if(c == '#'){
-            trie.insert(input.toString(), 1);
-            input = new StringBuilder();
-            return Arrays.asList();
+        if(c == '#') {
+            if(cur.sentence != null) {
+                cur.hot++;
+            } else {
+                cur.sentence = sentence;
+                cur.hot = 1;
+            }
+            sentence = "";
+            cur = root;
+            return new ArrayList<>();
+        } else {
+            cur.children.putIfAbsent(c, new Node());
+            cur = cur.children.get(c);
+            sentence += c;
+
+            PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.hot == y.hot ? y.sentence.compareTo(x.sentence) : x.hot - y.hot);
+            Node node = cur;
+            search(node, pq);
+            List<Node> tmp = new ArrayList<>(pq);
+            tmp.sort((x, y) -> x.hot == y.hot ? y.sentence.compareTo(x.sentence) : x.hot - y.hot);
+
+            List<String> res = new ArrayList<>();
+            for(int i = tmp.size() - 1; i >= 0; i--) {
+                res.add(tmp.get(i).sentence);
+            }
+            return res;
         }
-        else{
-            input.append(c);
-            return trie.startsWith(input.toString());
+    }
+
+    private void search(Node cur, PriorityQueue<Node> pq) {
+        if(cur == null) return;
+        if(cur.sentence != null) {
+            pq.offer(cur);
+            if(pq.size() > 3) {
+                pq.poll();
+            }
+        }
+        for(Node child: cur.children.values()) {
+            search(child, pq);
         }
     }
 }
