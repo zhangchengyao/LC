cb0d15a540a17f5b23e2e55abbb1bedefcb2bc7b
chengyaoZhang <tc2002618@126.com>
2019-08-11 12:33:37 -0700

    LC 694 modify

--- src/LC694_NumberOfDistinctIslands.java
+++ src/LC694_NumberOfDistinctIslands.java
@@ -2,25 +2,22 @@ import java.util.HashSet;
 
 public class LC694_NumberOfDistinctIslands {
     public int numDistinctIslands(int[][] grid) {
-        if(grid==null || grid.length==0 || grid[0].length==0) return 0;
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         HashSet<String> shapes = new HashSet<>();
-        int res = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1 && !visited[i][j]){
                     String str = dfs(grid, i, j, 'S', visited);
-                    if(!shapes.contains(str)){
-                        res++;
-                        shapes.add(str);
-                    }
+                    shapes.add(str);
                 }
             }
         }
-        return res;
+        return shapes.size();
     }
     private String dfs(int[][] grid, int i, int j, char direc, boolean[][] visited){
-        if(i<0 || j<0 || i>=grid.length || j>= grid[0].length || grid[i][j]==0 || visited[i][j]) return "E";
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0 || visited[i][j]) return "E";
         StringBuilder sb = new StringBuilder();
         sb.append(direc);
         visited[i][j] = true;
