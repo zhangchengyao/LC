1a6307f9359158fd2889e52e7e6a5e0cf1d0a225
chengyaoZhang <tc2002618@126.com>
2019-08-11 11:19:35 -0700

    LC 681 modify

--- src/LC681_NextClosestTime.java
+++ src/LC681_NextClosestTime.java
@@ -3,29 +3,30 @@ import java.util.TreeSet;
 public class LC681_NextClosestTime {
     public String nextClosestTime(String time) {
         char[] digits = new char[]{time.charAt(0), time.charAt(1), time.charAt(3), time.charAt(4)};
-        TreeSet<Character> set = new TreeSet();
+        TreeSet<Character> set = new TreeSet<>();
         for(char c: digits) set.add(c);
-        for(int i=digits.length-1;i>=0;i--){
-            if(set.higher(digits[i])!=null){
+
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(set.higher(digits[i]) != null){
                 char c = set.higher(digits[i]);
-                if(i==2&&c>='6') continue;
-                if(i==1&&time.charAt(0)=='2'&&c>='4') continue;
-                if(i==0&&c>='3') continue;
+                if(i == 2 && c >= '6') continue;
+                if(i == 1 && time.charAt(0) == '2' && c >= '4') continue;
+                if(i == 0 && c >= '3') continue;
                 StringBuilder res = new StringBuilder(time);
-                if(i>1){
+                if(i > 1){
                     res.setCharAt(++i, c);
-                    while(++i<time.length()) res.setCharAt(i, set.first());
+                    while(++i < time.length()) res.setCharAt(i, set.first());
                 }
                 else{
                     res.setCharAt(i, c);
-                    while(++i<time.length())
-                        if(time.charAt(i)!=':')
+                    while(++i < time.length())
+                        if(time.charAt(i) != ':')
                             res.setCharAt(i, set.first());
                 }
                 return res.toString();
             }
         }
         char c = set.first();
-        return c+""+c+":"+c+c;
+        return String.format("%d%d:%d%d", c, c, c, c);
     }
 }
