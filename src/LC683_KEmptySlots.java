4eb8dcbbdf2229536a512c7f92c14b2d3f23d195
chengyaoZhang <tc2002618@126.com>
2019-08-11 11:46:12 -0700

    LC 683 modify

--- src/LC683_KEmptySlots.java
+++ src/LC683_KEmptySlots.java
@@ -1,28 +1,28 @@
 public class LC683_KEmptySlots {
     public int kEmptySlots(int[] flowers, int k) {
         int[] slots = new int[flowers.length]; // slots[i] denotes which day the flower at position i will bloom
-        for(int i=0;i<flowers.length;i++){
-            slots[flowers[i]-1] = i+1;
+        for(int i = 0; i < flowers.length; i++){
+            slots[flowers[i] - 1] = i + 1;
         }
         int min = Integer.MAX_VALUE;
         int left = 0;
-        int right = left+k+1;
+        int right = left + k + 1;
         int i = left + 1;
-        while(right<slots.length){
-            if(i==right){
+        while(right < slots.length){
+            if(i == right){
                 min = Math.min(min, Math.max(slots[left], slots[right]));
                 left++;
                 right++;
                 i = left+1;
             }
             else{
-                if(slots[i]<Math.max(slots[left], slots[right])){
+                if(slots[i] < Math.max(slots[left], slots[right])){
                     left = i;
-                    right = left+k+1;
+                    right = left + k + 1;
                 }
                 i++;
             }
         }
-        return min==Integer.MAX_VALUE?-1:min;
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
 }
