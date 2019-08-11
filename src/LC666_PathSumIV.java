79614bfbdd6cf0a13149932fda7c8f3aea92f23d
chengyaoZhang <tc2002618@126.com>
2019-08-10 17:33:36 -0700

    LC 666

---
+++ src/LC666_PathSumIV.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class LC666_PathSumIV {
+    private int res = 0;
+
+    public int pathSum(int[] nums) {
+        HashMap<Integer, Integer> treeMap = new HashMap<>();
+        for(int n: nums) {
+            treeMap.put(n / 10, n % 10);
+        }
+
+        getSum(11, treeMap, 0);
+        return res;
+    }
+
+    private void getSum(int pos, HashMap<Integer, Integer> treeMap, int sum) {
+        if(!treeMap.containsKey(pos)) return ;
+
+        sum += treeMap.get(pos);
+        int left = (pos / 10 + 1) * 10 + (pos % 10) * 2 - 1;
+        int right = (pos / 10 + 1) * 10 + (pos % 10) * 2;
+        if(!treeMap.containsKey(left) && !treeMap.containsKey(right)) {
+            res += sum;
+        } else {
+            getSum(left, treeMap, sum);
+            getSum(right, treeMap, sum);
+        }
+    }
+}
