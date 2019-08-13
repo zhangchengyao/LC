6b6cadb3c711903780033d0aa901a1632396244c
chengyaoZhang <tc2002618@126.com>
2019-08-12 22:16:46 -0700

    LC 723 modify

--- src/LC723_CandyCrush.java
+++ src/LC723_CandyCrush.java
@@ -1,70 +1,57 @@
 public class LC723_CandyCrush {
     public int[][] candyCrush(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
         boolean stable = false;
-        while(!stable){
+        while(!stable) {
             stable = true;
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board[0].length;j++){
-                    if(board[i][j]==0 || (i>0 && Math.abs(board[i][j])==Math.abs(board[i-1][j]))) continue;
-                    int r = i+1;
-                    while(r<board.length && Math.abs(board[r][j])==Math.abs(board[i][j])) r++;
-                    for(int k=i;k<r;k++){
-                        if(probeRow(board, k, j)) stable = false;
-                        if(r-i>=3){
-                            stable = false;
-                            if(board[k][j]>0) board[k][j] = -board[k][j];
+
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    // check row
+                    int t = j + 1;
+                    while(t < n && Math.abs(board[i][t]) == Math.abs(board[i][j])) t++;
+                    if(t - j >= 3) {
+                        for(int k = t - 1; k >= j; k--) {
+                            if(board[i][k] > 0) board[i][k] = -board[i][k];
+                        }
+                    }
+                    // check column
+                    t = i + 1;
+                    while(t < m && Math.abs(board[t][j]) == Math.abs(board[i][j])) t++;
+                    if(t - i >= 3) {
+                        for(int k = t - 1; k >= i; k--) {
+                            if(board[k][j] > 0) board[k][j] = -board[k][j];
                         }
                     }
                 }
             }
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board[0].length;j++){
-                    if(board[i][j]<0) board[i][j]=0;
+
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(board[i][j] < 0) {
+                        stable = false;
+                        board[i][j] = 0;
+                    }
                 }
             }
 
-            update(board);
+            for(int j = 0; j < n; j++) {
+                int i = m - 1;
+                while(i >= 0 && board[i][j] > 0) i--;
+                if(i > 0) {
+                    int t = i - 1;
+                    while(t >= 0) {
+                        if(board[t][j] > 0) {
+                            board[i--][j] = board[t][j];
+                            board[t][j] = 0;
+                        }
+                        t--;
+                    }
+                }
+            }
         }
+
         return board;
-    }
-    private boolean probeRow(int[][] board, int i, int j){
-        int cnt = 1;
-        int k = j-1;
-        while(k>=0 && Math.abs(board[i][k])==Math.abs(board[i][j])){
-            cnt++;
-            k--;
-        }
-        int t = j+1;
-        while(t<board[0].length && Math.abs(board[i][t])==Math.abs(board[i][j])){
-            cnt++;
-            t++;
-        }
-        if(cnt>=3){
-            for(int y=k+1;y<t;y++){
-                if(board[i][y]>0) board[i][y] = -board[i][y];
-            }
-            return true;
-        }
-        return false;
-    }
-    private void update(int[][] board){
-        for(int c=0;c<board[0].length;c++){
-            int bottom = board.length-1;
-            while(bottom>=0 && board[bottom][c]!=0) bottom--;
-            while(bottom>=0){
-                int start = bottom-1;
-                while(start>=0 && board[start][c]==0) start--;
-                if(start<0) break;
-                int top = start-1;
-                while(top>=0 && board[top][c]!=0) top--;
-                for(int i=0;i<start-top;i++){
-                    board[bottom-i][c] = board[start-i][c];
-                }
-                for(int i=bottom-(start-top);i>top;i--){
-                    board[i][c] = 0;
-                }
-                bottom = bottom-(start-top);
-            }
-        }
     }
 }
