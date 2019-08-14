d5ccf6c74f16444797b8f4436bab7d2e81d1af22
chengyaoZhang <tc2002618@126.com>
2019-08-13 22:44:12 -0700

    LC 737 modify

--- src/LC737_SentenceSimilarityII.java
+++ src/LC737_SentenceSimilarityII.java
@@ -1,32 +1,44 @@
 import java.util.HashMap;
+import java.util.List;
 
 public class LC737_SentenceSimilarityII {
-    public boolean areSentencesSimilarTwo(String[] words1, String[] words2, String[][] pairs) {
-        if(words1.length!=words2.length) return false;
+    public boolean areSentencesSimilarTwo(String[] words1, String[] words2, List<List<String>> pairs) {
+        if(words1.length != words2.length) return false;
+
         HashMap<String, String> parent = new HashMap<>();
-        for(String[] pair: pairs){
-            parent.put(pair[0], pair[0]);
-            parent.put(pair[1], pair[1]);
+        for(List<String> pair: pairs) {
+            String w1 = pair.get(0);
+            String w2 = pair.get(1);
+            parent.putIfAbsent(w1, w1);
+            parent.putIfAbsent(w2, w2);
         }
-        for(String[] pair: pairs){
-            union(pair[0], pair[1], parent);
+
+        for(List<String> pair: pairs) {
+            union(parent, pair.get(0), pair.get(1));
         }
-        for(int i=0;i<words1.length;i++){
-            if(!words1[i].equals(words2[i]) && !(parent.containsKey(words1[i])&&parent.containsKey(words2[i])&&find(words1[i], parent).equals(find(words2[i], parent)))) return false;
+
+        for(int i = 0; i < words1.length; i++) {
+            String w1 = words1[i];
+            String w2 = words2[i];
+            if(!w1.equals(w2) && !(parent.containsKey(w1) && parent.containsKey(w2) && find(parent, w1).equals(find(parent, w2)))) return false;
         }
+
         return true;
     }
-    private String find(String s, HashMap<String, String> parent){
-        while(!parent.get(s).equals(s)){
-            parent.put(s, parent.get(parent.get(s)));
-            s = parent.get(s);
-        }
-        return s;
-    }
-    private void union(String a, String b, HashMap<String, String> parent){
-        String root1 = find(a, parent);
-        String root2 = find(b, parent);
+
+    private void union(HashMap<String, String> parent, String a, String b) {
+        String root1 = find(parent, a);
+        String root2 = find(parent, b);
         if(root1.equals(root2)) return;
         parent.put(root1, root2);
+    }
+
+    private String find(HashMap<String, String> parent, String a) {
+        while(!a.equals(parent.get(a))) {
+            parent.put(a, parent.get(parent.get(a)));
+            a = parent.get(a);
+        }
+
+        return a;
     }
 }
