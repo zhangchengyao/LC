50d82a6c75b86b113177691a6b9e17276b79a436
chengyaoZhang <tc2002618@126.com>
2019-08-18 12:06:21 -0700

    LC 760

---
+++ src/LC760_FindAnagramMappings.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class LC760_FindAnagramMappings {
+    public int[] anagramMappings(int[] A, int[] B) {
+        HashMap<Integer, Stack<Integer>> map = new HashMap<>();
+        for(int i = 0; i < B.length; i++) {
+            map.putIfAbsent(B[i], new Stack<>());
+            map.get(B[i]).push(i);
+        }
+
+        int[] res = new int[A.length];
+        for(int i = 0; i < A.length; i++) {
+            res[i] = map.get(A[i]).pop();
+        }
+
+        return res;
+    }
+}
