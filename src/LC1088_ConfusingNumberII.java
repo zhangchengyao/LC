4c5df8f0b0985b244b21363d42d1a6e4ddc774fd
chengyaoZhang <tc2002618@126.com>
2019-09-07 15:46:23 -0700

    LC 1088 modify

--- src/LC1088_ConfusingNumberII.java
+++ src/LC1088_ConfusingNumberII.java
@@ -1,48 +1,71 @@
 public class LC1088_ConfusingNumberII {
     private int[] cands = {0, 1, 6, 8, 9};
+    private int[] candsMir = {0, 1, 9, 8, 6};
     private int count = 0;
 
     public int confusingNumberII(int N) {
 
-        countConfusing(Integer.toString(N), new StringBuilder());
+        countConfusing(0, 0, 1, N);
 
         return count;
     }
 
-    private void countConfusing(String N, StringBuilder num) {
-        if(num.length() == N.length()) {
-            if(num.toString().compareTo(N) <= 0 && isDifferent(num.toString())) count++;
-            return;
-        }
-        if(num.length() > 0 && isDifferent(num.toString())) count++;
+    private void countConfusing(long n, long mirror, long pow, int N) {
+        if(n > N) return;
+        if(n > 0 && n != mirror) count++;
 
-        for(int x: cands) {
-            if(x == 0 && num.length() == 0) continue;
-            num.append(x);
-            countConfusing(N, num);
-            num.deleteCharAt(num.length() - 1);
+        for(int i = 0; i < cands.length; i++) {
+            int x = cands[i];
+            int mir = candsMir[i];
+            if(x == 0 && n == 0) continue;
+            countConfusing(n * 10 + x, mir * pow + mirror, pow * 10, N);
         }
     }
 
-    private boolean isDifferent(String num) {
-        int i = 0;
-        int j = num.length() - 1;
-        while(i < j) {
-            if(num.charAt(i) == '6') {
-                if(num.charAt(j) != '9') return true;
-            } else if(num.charAt(i) == '9') {
-                if(num.charAt(j) != '6') return true;
-            } else {
-                if(num.charAt(j) != num.charAt(i)) return true;
-            }
-
-            i++;
-            j--;
-        }
-
-        if(i == j)
-            return num.charAt(i) == '6' || num.charAt(i) == '9';
-
-        return false;
-    }
+//    private int[] cands = {0, 1, 6, 8, 9};
+//    private int count = 0;
+//
+//    public int confusingNumberII(int N) {
+//
+//        countConfusing(Integer.toString(N), new StringBuilder());
+//
+//        return count;
+//    }
+//
+//    private void countConfusing(String N, StringBuilder num) {
+//        if(num.length() == N.length()) {
+//            if(num.toString().compareTo(N) <= 0 && isDifferent(num.toString())) count++;
+//            return;
+//        }
+//        if(num.length() > 0 && isDifferent(num.toString())) count++;
+//
+//        for(int x: cands) {
+//            if(x == 0 && num.length() == 0) continue;
+//            num.append(x);
+//            countConfusing(N, num);
+//            num.deleteCharAt(num.length() - 1);
+//        }
+//    }
+//
+//    private boolean isDifferent(String num) {
+//        int i = 0;
+//        int j = num.length() - 1;
+//        while(i < j) {
+//            if(num.charAt(i) == '6') {
+//                if(num.charAt(j) != '9') return true;
+//            } else if(num.charAt(i) == '9') {
+//                if(num.charAt(j) != '6') return true;
+//            } else {
+//                if(num.charAt(j) != num.charAt(i)) return true;
+//            }
+//
+//            i++;
+//            j--;
+//        }
+//
+//        if(i == j)
+//            return num.charAt(i) == '6' || num.charAt(i) == '9';
+//
+//        return false;
+//    }
 }
