c4c7fabf7f22d728425e3b8c9a2a0f3e772927c7
chengyaoZhang <tc2002618@126.com>
2019-09-13 23:28:38 -0700

    LC 669

---
+++ src/LC669_TrimABinarySearchTree.java
@@ -0,0 +1,22 @@
+public class LC669_TrimABinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null) return null;
+
+        if(root.val < L) {
+            return trimBST(root.right, L, R);
+        } else if(root.val > R) {
+            return trimBST(root.left, L, R);
+        } else {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        }
+    }
+}
