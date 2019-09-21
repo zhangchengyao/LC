71450e308de5d47aacd96b79f39b98ed6bf49c8a
chengyaoZhang <tc2002618@126.com>
2019-09-20 23:23:09 -0700

    LC710

---
+++ src/LC710_RandomPickWithBlacklist.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LC710_RandomPickWithBlacklist {
+    private HashMap<Integer, Integer> map;
+    private int size;
+
+    public LC710_RandomPickWithBlacklist(int N, int[] blacklist) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n: blacklist) set.add(n);
+
+        map = new HashMap<>();
+        size = N - blacklist.length;
+        int cur = size;
+        for(int blackNum: blacklist) {
+            if(blackNum < size) {
+                while(set.contains(cur)) cur++;
+                map.put(blackNum, cur++);
+            }
+        }
+    }
+
+    public int pick() {
+        int cand = (int)(Math.random() * size);
+        if(map.containsKey(cand)) return map.get(cand);
+        else return cand;
+    }
+}
