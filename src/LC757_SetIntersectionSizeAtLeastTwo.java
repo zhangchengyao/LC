646254afc2a5a9f9a8a19eaa1a91bef48b7f3af1
chengyaoZhang <tc2002618@126.com>
2019-09-22 17:30:53 -0700

    LC757

---
+++ src/LC757_SetIntersectionSizeAtLeastTwo.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class LC757_SetIntersectionSizeAtLeastTwo {
+    public int intersectionSizeTwo(int[][] intervals) {
+        // Greedy algorithm
+        Arrays.sort(intervals, (a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+
+        int size = 0;
+        int l2 = -1;
+        int l1 = -1;
+        for(int[] interval: intervals) {
+            if(l1 < interval[0]) {
+                l2 = interval[interval.length - 1] - 1;
+                l1 = interval[interval.length - 1];
+                size += 2;
+            } else if(l2 < interval[0]) {
+                l2 = l1;
+                l1 = interval[interval.length - 1];
+                size++;
+            }
+        }
+
+        return size;
+    }
+}
