fdcab7c7080541dc0c49cc642eb8391e190d7b7d
chengyaoZhang <tc2002618@126.com>
2019-09-24 16:02:22 -0700

    LC789

---
+++ src/LC789_EscapeTheGhosts.java
@@ -0,0 +1,10 @@
+public class LC789_EscapeTheGhosts {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int dist = Math.abs(target[0]) + Math.abs(target[1]);
+        for(int[] ghost: ghosts) {
+            if(Math.abs(ghost[0] - target[0]) + Math.abs(ghost[1] - target[1]) <= dist) return false;
+        }
+
+        return true;
+    }
+}
