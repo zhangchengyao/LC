195604d6efd3e35b557f7794cc10f154ea99e9e9
chengyaoZhang <tc2002618@126.com>
2019-09-25 11:29:45 -0700

    LC 769 modify

--- src/LC769_MaxChunksToMakeSorted.java
+++ src/LC769_MaxChunksToMakeSorted.java
@@ -1,16 +1,14 @@
 public class LC769_MaxChunksToMakeSorted {
     public int maxChunksToSorted(int[] arr) {
-        if(arr==null || arr.length==0) return 0;
+        if(arr == null || arr.length == 0) return 0;
         int res = 0;
-        int p = 0;
         int curMax = -1;
-        while(p<arr.length){
+        for(int p = 0; p < arr.length; p++) {
             curMax = Math.max(curMax, arr[p]);
-            if(p==curMax){
+            if(p == curMax){
                 res++;
                 curMax = -1;
             }
-            p++;
         }
         return res;
     }
