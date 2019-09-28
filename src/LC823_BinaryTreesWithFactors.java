ad4a67e0d7bd1752c6cb9e89ec34b92607683c2d
chengyaoZhang <tc2002618@126.com>
2019-09-28 11:59:03 -0700

    LC 823 modify

--- src/LC823_BinaryTreesWithFactors.java
+++ src/LC823_BinaryTreesWithFactors.java
@@ -1,9 +1,30 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LC823_BinaryTreesWithFactors {
+//    public int numFactoredBinaryTrees(int[] A) {
+//        Arrays.sort(A);
+//        long[] opts = new long[A.length];
+//        Arrays.fill(opts, 1);
+//
+//        long res = 1;
+//        for(int i = 1; i < A.length; i++) {
+//            for(int j = 0; j < i; j++) {
+//                if(A[i] % A[j] == 0) {
+//                    int factor = A[i] / A[j];
+//                    int idx = Arrays.binarySearch(A, factor);
+//                    if(idx >= 0) {
+//                        opts[i] += opts[j] * opts[idx] % mod;
+//                    }
+//                }
+//            }
+//
+//            res += opts[i];
+//            res %= mod;
+//        }
+//
+//        return (int)res;
+//    }
+
     private int mod = 1000000007;
     private Map<Integer, Integer> memo = new HashMap<>();
 
