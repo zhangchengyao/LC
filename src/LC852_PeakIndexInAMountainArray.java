f9b717c2eeb791ce7461a929592d9450205837d2
chengyaoZhang <tc2002618@126.com>
2019-10-02 20:49:35 -0700

    LC 852 modify

--- src/LC852_PeakIndexInAMountainArray.java
+++ src/LC852_PeakIndexInAMountainArray.java
@@ -1,9 +1,22 @@
 public class LC852_PeakIndexInAMountainArray {
     public int peakIndexInMountainArray(int[] A) {
-        int i = 0;
-        for(;i<A.length;i++){
-            if(A[i]>A[i+1]) break;
+        int left = 0;
+        int right = A.length - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) return mid;
+            else if(A[mid] > A[mid - 1]) left = mid + 1;
+            else right = mid;
         }
-        return i;
+
+        return left;
     }
+
+//    public int peakIndexInMountainArray(int[] A) {
+//        int i = 0;
+//        for(; i < A.length; i++){
+//            if(A[i] > A[i+1]) break;
+//        }
+//        return i;
+//    }
 }
