93440ca9fe165b6c435b3a1b98cff7da531e3206
chengyaoZhang <tc2002618@126.com>
2019-10-02 21:20:55 -0700

    LC 853 modify

--- src/LC853_CarFleet.java
+++ src/LC853_CarFleet.java
@@ -3,18 +3,21 @@ import java.util.Arrays;
 public class LC853_CarFleet {
     public int carFleet(int target, int[] position, int[] speed) {
         int[][] cars = new int[position.length][2];
-        for(int i=0;i<position.length;i++){
+        for(int i = 0; i < position.length; i++){
             cars[i][0] = position[i];
             cars[i][1] = speed[i];
         }
         Arrays.sort(cars, (a, b) -> b[0]-a[0]);
-        int res = 0;
-        for(int i=0;i<cars.length;i++){
-            int p = i+1;
-            while(p<cars.length && cars[p][1]>cars[i][1] && (target-cars[i][0])*1.0/cars[i][1]>=(target-cars[p][0])*1.0/cars[p][1]) p++;
-            res++;
-            i = p-1;
+
+        int count = 0;
+        for(int i = 0; i < cars.length; i++) {
+            int j = i + 1;
+            while(j < cars.length && (target - cars[i][0]) * 1.0 / cars[i][1] >= (target - cars[j][0]) * 1.0 / cars[j][1]) j++;
+            count++;
+
+            i = j - 1;
         }
-        return res;
+
+        return count;
     }
 }
