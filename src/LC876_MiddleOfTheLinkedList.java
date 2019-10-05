f98e5d2e70ff8841680f1f71156382ef4fa115f6
chengyaoZhang <tc2002618@126.com>
2019-10-05 09:09:46 -0700

    LC 876

---
+++ src/LC876_MiddleOfTheLinkedList.java
@@ -0,0 +1,19 @@
+public class LC876_MiddleOfTheLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return fast.next == null ? slow : slow.next;
+    }
+}
