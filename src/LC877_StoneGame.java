fbe621744665104299843b7638414b8da0b7522b
chengyaoZhang <tc2002618@126.com>
2019-10-05 09:41:49 -0700

    LC 877

--- src/LC877_StoneGame.java
+++ src/LC877_StoneGame.java
@@ -1,12 +1,14 @@
 public class LC877_StoneGame {
     public boolean stoneGame(int[] piles) {
         int[][] score = new int[piles.length][piles.length];
-        for(int i=0;i<score.length;i++) score[i][i] = piles[i];
-        for(int j=1;j<piles.length;j++){
-            for(int i=j-1;i>=0;i--){
-                score[i][j] = Math.max(piles[i]-score[i+1][j], piles[j]-score[i][j-1]);
+        for(int i = 0; i < score.length; i++) score[i][i] = piles[i];
+
+        for(int j = 1; j < piles.length; j++){
+            for(int i = j - 1; i >= 0; i--){
+                score[i][j] = Math.max(piles[i] - score[i + 1][j], piles[j] - score[i][j - 1]);
             }
         }
-        return score[0][piles.length-1]>0;
+
+        return score[0][piles.length - 1] > 0;
     }
 }
