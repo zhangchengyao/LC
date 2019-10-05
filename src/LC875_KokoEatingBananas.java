79eff7c1daea41c4145826983e4297ae6bbf63f8
chengyaoZhang <tc2002618@126.com>
2019-10-05 08:54:04 -0700

    LC 875

--- src/LC875_KokoEatingBananas.java
+++ src/LC875_KokoEatingBananas.java
@@ -1,18 +1,21 @@
 public class LC875_KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int H) {
-        int max = Integer.MIN_VALUE;
-        for(int num: piles) max = Math.max(max, num);
-        int left = 1;
-        int right = max;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            int totalH = 0;
-            for(int num: piles){
-                totalH += num%mid==0?num/mid:num/mid+1;
+        int left = 0;
+        int right = piles[0];
+        for(int pile: piles) right = Math.max(right, pile);
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            int time = 0;
+            for(int pile: piles) {
+                time += (int)Math.ceil(pile * 1.0 / mid);
             }
-            if(totalH>H) left = mid+1;
-            else right = mid-1;
+
+            if(time > H) left = mid + 1;
+            else right = mid;
         }
+
         return left;
     }
 }
