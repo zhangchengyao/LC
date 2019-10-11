b76d0f0c58472ba57930df840f56bb8c622ffc3e
chengyaoZhang <tc2002618@126.com>
2019-10-11 16:14:18 -0700

    LC 890

--- src/LC890_FindAndReplacePattern.java
+++ src/LC890_FindAndReplacePattern.java
@@ -4,28 +4,26 @@ import java.util.List;
 
 public class LC890_FindAndReplacePattern {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-
         List<String> res = new ArrayList<>();
-        if(words==null || words.length==0) return res;
+        if(words == null || words.length == 0) return res;
 
         HashMap<Character, Character> map;
 
         for(String word: words){
-            if(word.length()!=pattern.length()) continue;
+            if(word.length() != pattern.length()) continue;
             map = new HashMap<>();
 
             int i = 0;
-            for(;i<pattern.length();i++){
-                if(map.containsKey(word.charAt(i))){
-                    if(map.get(word.charAt(i))!=pattern.charAt(i)) break;
-                }
-                else{
+            for(; i < pattern.length(); i++){
+                if(map.containsKey(word.charAt(i))) {
+                    if(map.get(word.charAt(i)) != pattern.charAt(i)) break;
+                } else {
                     if(map.containsValue(pattern.charAt(i))) break;
                     map.put(word.charAt(i), pattern.charAt(i));
                 }
             }
 
-            if(i==pattern.length()) res.add(word);
+            if(i == pattern.length()) res.add(word);
         }
         return res;
     }
