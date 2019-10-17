a91a6effb81551e6b23968bebb8a762467d23b6c
chengyaoZhang <tc2002618@126.com>
2019-10-17 12:22:33 -0700

    LC 801 modify

--- src/LC801_MinimumSwapsToMakeSequencesIncreasing.java
+++ src/LC801_MinimumSwapsToMakeSequencesIncreasing.java
@@ -3,20 +3,17 @@ import java.util.Arrays;
 public class LC801_MinimumSwapsToMakeSequencesIncreasing {
     public int minSwap(int[] A, int[] B) {
         int[][] dp = new int[A.length][2];
-        for(int i = 1; i < A.length; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE - 100);
-        }
         dp[0][1] = 1;
 
         for(int i = 1; i < A.length; i++) {
+            dp[i][0] = Integer.MAX_VALUE / 2;
+            dp[i][1] = Integer.MAX_VALUE / 2;
+
             if(A[i] > A[i - 1] && B[i] > B[i - 1]) {
-                dp[i][0] = dp[i - 1][0];
-                dp[i][1] = dp[i - 1][1] + 1;
-                if(A[i] > B[i - 1] && B[i] > A[i - 1]) {
-                    dp[i][0] = Math.min(dp[i][0], dp[i - 1][1]);
-                    dp[i][1] = Math.min(dp[i][1], dp[i - 1][0] + 1);
-                }
-            } else {
+                dp[i][0] = Math.min(dp[i][0], dp[i - 1][0]);
+                dp[i][1] = Math.min(dp[i][1], dp[i - 1][1] + 1);
+            }
+            if(A[i] > B[i - 1] && B[i] > A[i - 1]) {
                 dp[i][0] = Math.min(dp[i][0], dp[i - 1][1]);
                 dp[i][1] = Math.min(dp[i][1], dp[i - 1][0] + 1);
             }
