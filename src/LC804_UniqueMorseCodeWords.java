48e59c603cebbe65f95b01ce01894394e5ee4e3f
chengyaoZhang <tc2002618@126.com>
2019-10-17 16:39:18 -0700

    LC 804 modify

--- src/LC804_UniqueMorseCodeWords.java
+++ src/LC804_UniqueMorseCodeWords.java
@@ -1,15 +1,19 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class LC804_UniqueMorseCodeWords {
     public int uniqueMorseRepresentations(String[] words) {
-
-        String[] codes = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        HashSet<String> set = new HashSet<>();
+        String[] codes = new String[]{".-", "-...", "-.-.", "-..", ".",
+                                    "..-.", "--.", "....", "..", ".---",
+                                    "-.-", ".-..", "--", "-.", "---",
+                                    ".--.", "--.-", ".-.", "...", "-",
+                                    "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> set = new HashSet<>();
 
         for(String word: words){
             StringBuilder sb = new StringBuilder();
             for(char c: word.toCharArray()){
-                sb.append(codes[c-'a']);
+                sb.append(codes[c - 'a']);
             }
             set.add(sb.toString());
         }
