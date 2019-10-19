4d6ff89d45bdb3a3a45bf78016b166bb1d8b193d
chengyaoZhang <tc2002618@126.com>
2019-10-19 14:51:29 -0700

    LC 819 modify

--- src/LC819_MostCommonWord.java
+++ src/LC819_MostCommonWord.java
@@ -1,36 +1,29 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class LC819_MostCommonWord {
     public String mostCommonWord(String paragraph, String[] banned) {
-        HashSet<String> set = new HashSet<>();
-        for(String word: banned){
-            set.add(word.toLowerCase());
-        }
+        Set<String> banSet = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
 
-        HashMap<String, Integer> count = new HashMap<>();
-        int max = 0;
-        String res = "";
+        String mostComm = null;
+
         int i = 0;
-        while(i<paragraph.length()){
-            if(!Character.isLetter(paragraph.charAt(i))) i++;
-            else {
+        while(i < paragraph.length()) {
+            while(i < paragraph.length() && !Character.isLetter(paragraph.charAt(i))) i++;
+
+            if(i < paragraph.length()) {
                 int j = i + 1;
-                while(j<paragraph.length() && Character.isLetter(paragraph.charAt(j))) j++;
+                while(j < paragraph.length() && Character.isLetter(paragraph.charAt(j))) j++;
 
                 String word = paragraph.substring(i, j).toLowerCase();
-                if(!set.contains(word)){
-                    count.put(word, count.getOrDefault(word, 0)+1);
-                    if(count.get(word)>max){
-                        max = count.get(word);
-                        res = word;
-                    }
+                if(!banSet.contains(word)) {
+                    count.put(word, count.getOrDefault(word, 0) + 1);
+                    if(mostComm == null || count.get(word) > count.get(mostComm)) mostComm = word;
                 }
-
-                i = j + 1;
+                i = j;
             }
         }
 
-        return res;
+        return mostComm;
     }
 }
