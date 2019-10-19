ac6f5e4012bdc20247216e952755a944c74fa825
chengyaoZhang <tc2002618@126.com>
2019-10-19 10:03:10 -0700

    LC 809 modify

--- src/LC809_ExpressiveWords.java
+++ src/LC809_ExpressiveWords.java
@@ -4,21 +4,19 @@ public class LC809_ExpressiveWords {
         for(String word: words){
             int i = 0;
             int j = 0;
-            while(i<S.length() && j<word.length()){
-                if(S.charAt(i)==word.charAt(j)){
-                    int p = i+1;
-                    int q = j+1;
-                    while(p<S.length() && S.charAt(p)==S.charAt(i)) p++;
-                    while(q<word.length() && word.charAt(q)==word.charAt(j)) q++;
-                    if(p-i==q-j || (p-i>=3&&p-i>q-j)){
+            while(i < S.length() && j < word.length()){
+                if(S.charAt(i) == word.charAt(j)){
+                    int p = i + 1;
+                    int q = j + 1;
+                    while(p < S.length() && S.charAt(p) == S.charAt(i)) p++;
+                    while(q < word.length() && word.charAt(q) == word.charAt(j)) q++;
+                    if(p - i == q - j || (p - i >= 3 && p - i > q - j)){
                         i = p;
                         j = q;
-                    }
-                    else break;
-                }
-                else break;
+                    } else break;
+                } else break;
             }
-            if(i==S.length() && j==word.length()) res++;
+            if(i == S.length() && j == word.length()) res++;
         }
         return res;
     }
