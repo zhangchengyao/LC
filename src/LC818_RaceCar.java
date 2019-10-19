b4b69538e330323efade840c559d6f45eb32b92c
chengyaoZhang <tc2002618@126.com>
2019-10-19 14:38:15 -0700

    LC 818 modify

--- src/LC818_RaceCar.java
+++ src/LC818_RaceCar.java
@@ -1,13 +1,22 @@
 public class LC818_RaceCar {
-    int[] min = new int[10001];
+    private int[] min = new int[10001];
+
     public int racecar(int target) {
-        if(min[target]>0) return min[target];
-        int n = (int)Math.floor(Math.log(target+1.0)/Math.log(2.0));
-        if((1<<n)==target+1) min[target] = n;
-        else{
-            min[target] = n+2+racecar((1<<(n+1))-1-target);
-            for(int i=0;i<n;i++){
-                min[target] = Math.min(min[target], racecar(target-(1<<n)+(1<<i))+n+i+2);
+        if(min[target] != 0) return min[target];
+
+        int n = 1;
+        while((1 << n) - 1 < target) {
+            n++;
+        }
+
+        if((1 << n) - 1 == target) {
+            min[target] = n;
+        } else {
+            min[target] = racecar((1 << n) - 1 - target) + n + 1;
+            n--;
+            int dist = target - ((1 << n) - 1);
+            for(int m = 0; m < n; m++) {
+                min[target] = Math.min(min[target], n + racecar(dist + ((1 << m) - 1)) + m + 2);
             }
         }
         return min[target];
