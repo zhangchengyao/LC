f5908c2829d76b1074b767b837a494173dbdffc8
chengyaoZhang <tc2002618@126.com>
2019-10-20 10:46:40 -0700

    LC 830 modify

--- src/LC830_PositionsOfLargeGroups.java
+++ src/LC830_PositionsOfLargeGroups.java
@@ -6,12 +6,12 @@ public class LC830_PositionsOfLargeGroups {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> tmp = new ArrayList<>();
         int i = 0;
-        while(i<S.length()){
-            int j = i+1;
-            while(j<S.length() && S.charAt(j)==S.charAt(j-1)) j++;
-            if(j-i>=3){
+        while(i < S.length()){
+            int j = i + 1;
+            while(j < S.length() && S.charAt(j) == S.charAt(j - 1)) j++;
+            if(j - i >= 3){
                 tmp.add(i);
-                tmp.add(j-1);
+                tmp.add(j - 1);
                 res.add(new ArrayList<>(tmp));
                 tmp.clear();
             }
