c8c44a49dc942dbb5512bc6bbcc301478e9400ff
chengyaoZhang <tc2002618@126.com>
2019-10-21 23:32:43 -0700

    LC 833 modify

--- src/LC833_FindAndReplaceInString.java
+++ src/LC833_FindAndReplaceInString.java
@@ -2,29 +2,25 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class LC833_FindAndReplaceInString {
-    class Operation{
-        int index;
-        String src;
-        String target;
-        Operation(int index, String src, String target){
-            this.index = index;
-            this.src = src;
-            this.target = target;
-        }
-    }
     public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
-        StringBuilder sb = new StringBuilder(S);
-        Operation[] opes = new Operation[indexes.length];
-        for(int i=0;i<indexes.length;i++){
-            opes[i] = new Operation(indexes[i], sources[i], targets[i]);
+        int[] replaces = new int[S.length()];
+        Arrays.fill(replaces, -1);
+
+        for(int i = 0; i < indexes.length; i++) {
+            int idx = indexes[i];
+            if(S.substring(idx, idx + sources[i].length()).equals(sources[i])) replaces[idx] = i;
         }
-        Arrays.sort(opes, (a,b) -> b.index-a.index);
-        for(Operation o: opes){
-            if(sb.substring(o.index, o.index+o.src.length()).equals(o.src)){
-                for(int i=0;i<o.src.length();i++) sb.deleteCharAt(o.index);
-                sb.insert(o.index, o.target);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < replaces.length; i++) {
+            if(replaces[i] != -1) {
+                sb.append(targets[replaces[i]]);
+                i += sources[replaces[i]].length() - 1;
+            } else {
+                sb.append(S.charAt(i));
             }
         }
+
         return sb.toString();
     }
 }
