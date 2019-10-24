e12fd35917ebf906e123b38185df24fe49232515
chengyaoZhang <tc2002618@126.com>
2019-10-24 16:33:02 -0700

    LC 835 modify

--- src/LC835_ImageOverlap.java
+++ src/LC835_ImageOverlap.java
@@ -2,25 +2,30 @@ import java.util.HashMap;
 
 public class LC835_ImageOverlap {
     public int largestOverlap(int[][] A, int[][] B) {
-        HashMap<String, Integer> count = new HashMap<>();
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[0].length;j++){
-                if(A[i][j]==1){
-                    for(int m=0;m<B.length;m++){
-                        for(int n=0;n<B[0].length;n++){
-                            if(B[m][n]==1){
-                                String dist = (m-i)+" "+(n-j);
-                                count.put(dist, count.getOrDefault(dist, 0)+1);
-                            }
-                        }
+        int N = A.length;
+        int[][] dist = new int[N * 2][N * 2];
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(A[i][j] == 0) continue;
+
+                for(int s = 0; s < N; s++) {
+                    for(int t = 0; t < N; t++) {
+                        if(B[s][t] == 0) continue;
+
+                        dist[i - s + N][j - t + N]++;
                     }
                 }
             }
         }
+
         int res = 0;
-        for(int cnt: count.values()){
-            res = Math.max(res, cnt);
+        for(int i = 0; i < dist.length; i++) {
+            for(int j = 0; j < dist.length; j++) {
+                res = Math.max(res, dist[i][j]);
+            }
         }
+
         return res;
     }
 }
