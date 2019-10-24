90f2a468e0decc366df5231d9ef200d36c437810
chengyaoZhang <tc2002618@126.com>
2019-10-24 16:57:46 -0700

    LC 836 modify

--- src/LC836_RectangleOverlap.java
+++ src/LC836_RectangleOverlap.java
@@ -1,5 +1,5 @@
 public class LC836_RectangleOverlap {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return !(rec1[1]>=rec2[3] || rec1[3]<=rec2[1] || rec1[2]<=rec2[0] || rec1[0]>=rec2[2]);
+        return !(rec1[0] >= rec2[2] || rec1[1] >= rec2[3] || rec1[2] <= rec2[0] || rec1[3] <= rec2[1]);
     }
 }
