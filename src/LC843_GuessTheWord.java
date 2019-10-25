dbce5907b81263e0d939206bb79ca007e22b735f
chengyaoZhang <tc2002618@126.com>
2019-10-24 21:50:13 -0700

    LC 843 modify

--- src/LC843_GuessTheWord.java
+++ src/LC843_GuessTheWord.java
@@ -1,20 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LC843_GuessTheWord {
     interface Master {
         public int guess(String word);
     }
     public void findSecretWord(String[] wordlist, Master master) {
-        ArrayList<String> list = new ArrayList<>();
-        for(String str: wordlist) list.add(str);
-        for(int k=0;k<10;k++){
+        List<String> list = new ArrayList<>(Arrays.asList(wordlist));
+        for(int k = 0; k < 10; k++){
             if(list.isEmpty()) break;
-            int idx = (int)(Math.random()*list.size());
+
+            int idx = (int)(Math.random() * list.size());
             String selected = list.get(idx);
             int match = master.guess(selected);
-            if(match==6) break;
-            for(int i=0;i<list.size();i++){
-                if(list.get(i).equals(selected) || similar(selected, list.get(i))!=match){
+            if(match == 6) break;
+
+            for(int i = 0; i < list.size(); i++){
+                if(list.get(i).equals(selected) || similar(selected, list.get(i)) != match){
                     list.remove(i);
                     i--;
                 }
@@ -23,8 +26,8 @@ public class LC843_GuessTheWord {
     }
     private int similar(String word1, String word2){
         int cnt = 0;
-        for(int i=0;i<word1.length();i++){
-            if(word1.charAt(i)==word2.charAt(i)) cnt++;
+        for(int i = 0; i < word1.length(); i++){
+            if(word1.charAt(i) == word2.charAt(i)) cnt++;
         }
         return cnt;
     }
