05d4e7e95377f036533b90fc409a36d100e16286
chengyaoZhang <tc2002618@126.com>
2019-10-25 15:16:23 -0700

    LC 849 modify

--- src/LC849_MaximizeDistanceToClosestPerson.java
+++ src/LC849_MaximizeDistanceToClosestPerson.java
@@ -1,24 +1,18 @@
 public class LC849_MaximizeDistanceToClosestPerson {
     public int maxDistToClosest(int[] seats) {
-        int i = 0;
-        while(i<seats.length && seats[i]==0) i++;
-        int maxLen = i;
-        int j = seats.length-1;
-        while(j>=0 && seats[j]==0) j--;
-        if(seats.length-1-j>maxLen){
-            maxLen = seats.length-1-j;
-        }
-        int curLen = 0;
-        while(i<=j){
-            if(seats[i]==1){
-                if((curLen+1)/2>maxLen){
-                    maxLen = (curLen+1)/2;
+        int maxDist = 0;
+        int prev = -1;
+        for(int i = 0; i < seats.length; i++) {
+            if(seats[i] == 1) {
+                if(prev == -1) {
+                    maxDist = Math.max(maxDist, i);
+                } else {
+                    maxDist = Math.max(maxDist, (i - prev) / 2);
                 }
-                curLen = 0;
+                prev = i;
             }
-            else curLen++;
-            i++;
         }
-        return maxLen;
+        maxDist = Math.max(maxDist, seats.length - prev - 1);
+        return maxDist;
     }
 }
