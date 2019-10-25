7addfca64cc5ca32e1f2388feaac5fa74f8a87ea
chengyaoZhang <tc2002618@126.com>
2019-10-24 17:45:03 -0700

    LC 837 modify

--- src/LC837_New21Game.java
+++ src/LC837_New21Game.java
@@ -1,16 +1,16 @@
 public class LC837_New21Game {
     public double new21Game(int N, int K, int W) {
-        if(K>N) return 0.0;
-        double[] dp = new double[K+W];
-        double[] sum = new double[K+W+1];
-        for(int i=dp.length-1;i>=0;i--){
-            if(i>N) continue;
-            if(i>=K) dp[i] = 1.0;
-            else{
-                dp[i] += (sum[i+1]-sum[i+1+W])/W;
+        if(K > N) return 0.0;
+        double[] dp = new double[K + W];
+        double[] sum = new double[K + W + 1];
+        for(int i = Math.min(N, dp.length - 1); i >= 0; i--){
+            if(i >= K) dp[i] = 1.0;
+            else {
+                dp[i] += (sum[i + 1] - sum[i + 1 + W]) / W;
             }
-            sum[i] += dp[i]+sum[i+1];
+            sum[i] = dp[i] + sum[i + 1];
         }
+
         return dp[0];
     }
 }
