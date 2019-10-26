ed5c013eba50c6748d49a2eb7419717a209a58fb
chengyaoZhang <tc2002618@126.com>
2019-10-26 08:46:19 -0700

    LC 753 modify

--- src/LC753_CrackingTheSafe.java
+++ src/LC753_CrackingTheSafe.java
@@ -1,31 +1,33 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 public class LC753_CrackingTheSafe {
-    HashSet<String> set = new HashSet<>();
+    private Set<String> set = new HashSet<>();
+
     public String crackSafe(int n, int k) {
         StringBuilder res = new StringBuilder();
         check(res, n, k);
         return res.toString();
     }
+
     private boolean check(StringBuilder res, int n, int k){
-        if(res.length()==((int)Math.pow(k, n)+n-1)) return true;
-        String s = res.toString();
-        for(int i=0;i<k;i++){
+        if(res.length() == ((int)Math.pow(k, n) + n - 1)) return true;
+        for(int i = 0; i < k; i++){
             res.append(i);
-            if(res.length()>=n){
-                if(set.contains(res.substring(res.length()-n))){
-                    res.deleteCharAt(res.length()-1);
+            if(res.length() >= n){
+                if(set.contains(res.substring(res.length() - n))) {
+                    res.deleteCharAt(res.length() - 1);
                     continue;
                 }
-                set.add(res.substring(res.length()-n));
+                set.add(res.substring(res.length() - n));
             }
             boolean suc = check(res, n, k);
-            if(suc){
+            if(suc) {
                 return true;
-            }else{
-                set.remove(res.substring(res.length()-n));
-                res.deleteCharAt(res.length()-1);
+            } else {
+                set.remove(res.substring(res.length() - n));
+                res.deleteCharAt(res.length() - 1);
             }
         }
         return false;
