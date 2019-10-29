9c4e61f0964e4fef0edc9139decebae2fc0f2cf0
chengyaoZhang <tc2002618@126.com>
2019-10-28 17:21:10 -0700

    LC 777 modify

--- src/LC777_SwapAdjacentInLRString.java
+++ src/LC777_SwapAdjacentInLRString.java
@@ -1,17 +1,20 @@
 public class LC777_SwapAdjacentInLRString {
     public boolean canTransform(String start, String end) {
-        if(start.length()!=end.length()) return false;
         int i = 0;
         int j = 0;
-        while(i<start.length() && j<end.length()){
-            while(i<start.length() && start.charAt(i)=='X') i++;
-            while(j<end.length() && end.charAt(j)=='X') j++;
-            if(i==start.length()) return j==end.length();
-            if(j==end.length()) return i==start.length();
-            if(start.charAt(i)!=end.charAt(j)) return false;
-            else{
-                if((start.charAt(i)=='L' && i<j) || (start.charAt(i)=='R' && i>j)) return false;
+        while(i < start.length() && j < end.length()) {
+            while(i < start.length() && start.charAt(i) == 'X') i++;
+            while(j < end.length() && end.charAt(j) == 'X') j++;
+
+            if(i == start.length() && j == end.length()) return true;
+            if(i == start.length() || j == end.length()) return false;
+
+            if(start.charAt(i) == 'R') {
+                if(end.charAt(j) == 'L' || i > j) return false;
+            } else {
+                if(end.charAt(j) == 'R' || i < j) return false;
             }
+
             i++;
             j++;
         }
