7574d9b3d784bd125f6303f2cf344094c597b235
chengyaoZhang <tc2002618@126.com>
2019-10-31 22:38:39 -0700

    LC 703 modify

--- src/LC703_KthLargestElementInAStream.java
+++ src/LC703_KthLargestElementInAStream.java
@@ -1,24 +1,25 @@
 import java.util.PriorityQueue;
 
 public class LC703_KthLargestElementInAStream {
-    PriorityQueue<Integer> data;
-    int k;
+    private PriorityQueue<Integer> data;
+    private int k;
+
     public LC703_KthLargestElementInAStream(int k, int[] nums) {
         data = new PriorityQueue<>();
         this.k = k;
-        for(int num: nums){
+        for(int num: nums) {
             data.offer(num);
-            if(data.size() > k){
+            if(data.size() > k) {
                 data.poll();
             }
         }
     }
 
     public int add(int val) {
-        if(data.size()<k){
+        if(data.size() < k) {
             data.offer(val);
-        }else{
-            if(val>data.peek()){
+        } else {
+            if(val > data.peek()) {
                 data.poll();
                 data.offer(val);
             }
