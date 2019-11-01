cdcb3cf7d82464c4bc387c3bca879a5b8be035a7
chengyaoZhang <tc2002618@126.com>
2019-10-31 21:53:04 -0700

    LC 701 modify

--- src/LC701_InsertIntoABinarySearchTree.java
+++ src/LC701_InsertIntoABinarySearchTree.java
@@ -5,21 +5,20 @@ public class LC701_InsertIntoABinarySearchTree {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+
     public TreeNode insertIntoBST(TreeNode root, int val) {
         TreeNode cur = root;
-        while(true){
-            if(cur.val<val){
-                if(cur.right==null){
+        while(true) {
+            if(cur.val < val) {
+                if(cur.right == null) {
                     cur.right = new TreeNode(val);
                     break;
-                }
-                else cur = cur.right;
-            }else{
-                if(cur.left==null){
+                } else cur = cur.right;
+            } else {
+                if(cur.left == null) {
                     cur.left = new TreeNode(val);
                     break;
-                }
-                else cur = cur.left;
+                } else cur = cur.left;
             }
         }
         return root;
