6d3ad60899a65fe3c02b0f909554745dc90a015e
chengyaoZhang <tc2002618@126.com>
2019-10-31 19:17:18 -0700

    LC 792 modify

--- src/LC792_NumberOfMatchingSubsequences.java
+++ src/LC792_NumberOfMatchingSubsequences.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class LC792_NumberOfMatchingSubsequences {
     public int numMatchingSubseq(String S, String[] words) {
-        HashMap<Character, List<Integer>> map = new HashMap<>();
-        for(int i=0;i<S.length();i++){
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i < S.length(); i++){
             List<Integer> list = map.getOrDefault(S.charAt(i), new ArrayList<>());
             list.add(i);
             map.put(S.charAt(i), list);
@@ -16,18 +13,18 @@ public class LC792_NumberOfMatchingSubsequences {
         for(String word: words){
             if(isSubsequence(map, word)) res++;
         }
-        return res;
 
+        return res;
     }
-    private boolean isSubsequence(HashMap<Character, List<Integer>> map, String t){
+    private boolean isSubsequence(Map<Character, List<Integer>> map, String t){
         int cur = 0;
-        for(int i=0;i<t.length();i++){
+        for(int i = 0; i < t.length(); i++){
             List<Integer> list = map.get(t.charAt(i));
-            if(list==null) return false;
+            if(list == null) return false;
             int idx = Collections.binarySearch(list, cur);
-            if(idx<0) idx = -idx-1;
-            if(idx==list.size()) return false;
-            cur = list.get(idx)+1;
+            if(idx < 0) idx = -idx - 1;
+            if(idx == list.size()) return false;
+            cur = list.get(idx) + 1;
         }
         return true;
     }
