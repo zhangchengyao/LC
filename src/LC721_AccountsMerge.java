8682ca34f3aace84d83dc44fba18aa0d3c0f2de4
chengyaoZhang <tc2002618@126.com>
2019-11-02 13:35:44 -0700

    LC 721 modify

--- src/LC721_AccountsMerge.java
+++ src/LC721_AccountsMerge.java
@@ -2,49 +2,40 @@ import java.util.*;
 
 public class LC721_AccountsMerge {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        List<List<String>> res = new ArrayList<>();
-        HashMap<String, TreeSet<String>> rootToEmails = new HashMap<>();
-        HashMap<String, String> emailToName = new HashMap<>();
-        HashMap<String, String> parent = new HashMap<>();
-        for(List<String> list: accounts){
+        Map<String, String> emailToName = new HashMap<>();
+        Map<String, String> parent = new HashMap<>();
+        for(List<String> list: accounts) {
             String name = list.get(0);
-            for(int j=1;j<list.size();j++){
-                parent.put(list.get(j), list.get(j));
+            for(int j = 1; j < list.size(); j++) {
+                parent.putIfAbsent(list.get(j), list.get(j));
                 emailToName.put(list.get(j), name);
+                if(j > 1) union(list.get(1), list.get(j), parent);
             }
         }
-        for(List<String> list: accounts){
-            String email = list.get(1);
-            for(int j=2;j<list.size();j++){
-                union(email, list.get(j), parent);
+
+        Map<String, List<String>> mergedAccounts = new HashMap<>();
+        for(String email: emailToName.keySet()) {
+            String root = find(email, parent);
+            if(!mergedAccounts.containsKey(root)) {
+                mergedAccounts.put(root, new ArrayList<>());
+                mergedAccounts.get(root).add(emailToName.get(email));
             }
+            mergedAccounts.get(root).add(email);
         }
-        for(List<String> list: accounts){
-            for(int j=1;j<list.size();j++){
-                String root = find(list.get(j), parent);
-                TreeSet<String> ts = rootToEmails.getOrDefault(root, new TreeSet<>());
-                ts.add(list.get(j));
-                rootToEmails.put(root, ts);
-            }
-        }
-        for(Map.Entry<String, TreeSet<String>> entry: rootToEmails.entrySet()){
-            List<String> tmp = new ArrayList<>();
-            String name = emailToName.get(entry.getKey());
-            tmp.add(name);
-            tmp.addAll(entry.getValue());
-            res.add(tmp);
-        }
-        return res;
+
+        for(List<String> list: mergedAccounts.values()) Collections.sort(list);
+
+        return new ArrayList<>(mergedAccounts.values());
     }
 
-    private String find(String a, HashMap<String, String> parent){
+    private String find(String a, Map<String, String> parent){
         while(!parent.get(a).equals(a)){
             parent.put(a, parent.get(parent.get(a)));
             a = parent.get(a);
         }
         return a;
     }
-    private void union(String a, String b, HashMap<String, String> parent){
+    private void union(String a, String b, Map<String, String> parent){
         String root1 = find(a, parent);
         String root2 = find(b, parent);
         if(root1.equals(root2)) return ;
