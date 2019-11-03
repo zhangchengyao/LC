a42477c205a7482e1072d642ecc6dc9d64cd28a1
chengyaoZhang <tc2002618@126.com>
2019-11-02 19:49:47 -0700

    LC 722 modify

--- src/LC722_RemoveComments.java
+++ src/LC722_RemoveComments.java
@@ -7,27 +7,24 @@ public class LC722_RemoveComments {
         boolean block = false;
         StringBuilder sb = new StringBuilder();
         for(String src: source){
-            for(int i=0;i<src.length();i++){
-                if(block){
-                    if(i<src.length()-1 && src.substring(i, i+2).equals("*/")){
+            for(int i = 0; i < src.length(); i++) {
+                if(block) {
+                    if(i < src.length() - 1 && src.substring(i, i + 2).equals("*/")) {
                         block = false;
                         i++;
                     }
-                }
-                else{
-                    if(i<src.length()-1 && src.substring(i, i+2).equals("//")){
+                } else {
+                    if(i < src.length() - 1 && src.substring(i, i + 2).equals("//")) {
                         break;
-                    }
-                    else if(i<src.length()-1 && src.substring(i, i+2).equals("/*")){
+                    } else if(i < src.length() - 1 && src.substring(i, i + 2).equals("/*")) {
                         block = true;
                         i++;
-                    }
-                    else{
+                    } else{
                         sb.append(src.charAt(i));
                     }
                 }
             }
-            if(!block && sb.length()>0){
+            if(!block && sb.length() > 0) {
                 res.add(sb.toString());
                 sb = new StringBuilder();
             }
