8c8821e3fcf1d0067d19f5311cdf1f93596fae99
chengyaoZhang <tc2002618@126.com>
2019-11-03 16:52:30 -0800

    LC 738 modify

--- src/LC738_MonotoneIncreasingDigits.java
+++ src/LC738_MonotoneIncreasingDigits.java
@@ -1,15 +1,14 @@
 public class LC738_MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
-        StringBuilder sb = new StringBuilder(N+"");
+        StringBuilder sb = new StringBuilder(N + "");
         int j = sb.length();
-        for(int i=sb.length()-1;i>0;i--){
-            if(sb.charAt(i)<sb.charAt(i-1)) {
-                sb.setCharAt(i-1, (char)(sb.charAt(i-1)-1));
-                j = i-1;
+        for(int i = sb.length() - 1; i > 0; i--){
+            if(sb.charAt(i) < sb.charAt(i - 1)) {
+                sb.setCharAt(i - 1, (char)(sb.charAt(i - 1) - 1));
+                j = i;
             }
         }
-        for(int i=j+1;i<sb.length();i++) sb.setCharAt(i, '9');
-        if(sb.charAt(0)=='0') sb.deleteCharAt(0);
+        for(int i = j; i < sb.length(); i++) sb.setCharAt(i, '9');
         return Integer.parseInt(sb.toString());
     }
 }
