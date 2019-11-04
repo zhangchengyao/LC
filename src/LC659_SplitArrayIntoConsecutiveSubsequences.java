6b25d813695923a95fef4396820ee0f7318d1118
chengyaoZhang <tc2002618@126.com>
2019-11-04 15:01:30 -0800

    LC 659 modify

--- src/LC659_SplitArrayIntoConsecutiveSubsequences.java
+++ src/LC659_SplitArrayIntoConsecutiveSubsequences.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.List;
+import java.util.Map;
 
 public class LC659_SplitArrayIntoConsecutiveSubsequences {
     public boolean isPossible(int[] nums) {
-        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        int sequenceLess3 = 0;
-        for(int num: nums){
-            if(map.containsKey(num-1)){
-                PriorityQueue<Integer> minHeap = map.get(num-1);
-                int tmp = minHeap.poll()+1;
-                if(minHeap.isEmpty()) map.remove(num-1);
-                PriorityQueue<Integer> h = map.getOrDefault(num, new PriorityQueue<>());
-                h.offer(tmp);
-                map.put(num, h);
-                if(tmp==3) sequenceLess3--;
-            }else{
-                PriorityQueue<Integer> h = map.getOrDefault(num, new PriorityQueue<>());
-                h.offer(1);
-                map.put(num, h);
-                sequenceLess3++;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int smallSeq = 0;
+        for(int n: nums) {
+            List<Integer> list = map.get(n - 1);
+            map.putIfAbsent(n, new ArrayList<>());
+            if(list != null && list.size() > 0) {
+                int len = list.remove(list.size() - 1);
+                map.get(n).add(len + 1);
+                map.get(n).sort(Comparator.reverseOrder());
+                if(len == 2) smallSeq--;
+            } else {
+                map.get(n).add(1);
+                smallSeq++;
             }
         }
-        return sequenceLess3==0;
+
+        return smallSeq == 0;
     }
 }
