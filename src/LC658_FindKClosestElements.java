dd1dfe6843c8b8106538d3baeb2eddf318dd75a6
chengyaoZhang <tc2002618@126.com>
2019-11-04 14:47:49 -0800

    LC 658 modify

--- src/LC658_FindKClosestElements.java
+++ src/LC658_FindKClosestElements.java
@@ -6,13 +6,11 @@ public class LC658_FindKClosestElements {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> res = new ArrayList<>();
         int idx = Arrays.binarySearch(arr, x);
-        if(idx<0) idx = -idx-1;
-        if(idx>0 && x-arr[idx-1]<arr[idx]-x) idx--;
-        res.add(arr[idx]);
-        int i = idx-1;
-        int j = idx+1;
-        for(int cnt=1;cnt<k;cnt++){
-            if(i>=0 && (j>=arr.length || x-arr[i]<=arr[j]-x)) res.add(0, arr[i--]);
+        if(idx < 0) idx = -idx - 1;
+        int i = idx - 1;
+        int j = idx;
+        for(int cnt = 0; cnt < k; cnt++){
+            if(i >= 0 && (j >= arr.length || x - arr[i] <= arr[j] - x)) res.add(0, arr[i--]);
             else res.add(arr[j++]);
         }
         return res;
