4dff5e0435dfdb62bbaea9180e6c63514877a5a7
chengyaoZhang <tc2002618@126.com>
2019-11-03 23:05:41 -0800

    LC 743 modify

--- src/LC743_NetworkDelayTime.java
+++ src/LC743_NetworkDelayTime.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class LC743_NetworkDelayTime {
     class Edge{
@@ -13,33 +15,33 @@ public class LC743_NetworkDelayTime {
     }
 
     public int networkDelayTime(int[][] times, int N, int K) {
-        if(N==1) return 0;
+        if(N == 1) return 0;
         int res = Integer.MIN_VALUE;
 
-        ArrayList<Edge>[] graph = new ArrayList[N+1];
+        ArrayList<Edge>[] graph = new ArrayList[N + 1];
         for(int[] time: times){
-            if(graph[time[0]]==null) graph[time[0]] = new ArrayList<>();
+            if(graph[time[0]] == null) graph[time[0]] = new ArrayList<>();
             graph[time[0]].add(new Edge(time[1], time[2]));
         }
-        if(graph[K]==null) return -1;
+        if(graph[K] == null) return -1;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1]-b[1]);
-        HashSet<Integer> visited = new HashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        Set<Integer> visited = new HashSet<>();
         visited.add(K);
         for(Edge e: graph[K]) pq.offer(new int[]{e.dest, e.weight});
 
-        for(int i=0;i<N-1;i++){
+        for(int i = 0; i < N - 1; i++){
             int[] cur = pq.poll();
-            if(cur==null) return -1;
+            if(cur == null) return -1;
             while(!pq.isEmpty() && visited.contains(cur[0])) cur = pq.poll();
             if(visited.contains(cur[0])) return -1;
             visited.add(cur[0]);
-            if(graph[cur[0]]!=null){
+            if(graph[cur[0]] != null){
                 for(Edge e: graph[cur[0]]){
-                    pq.offer(new int[]{e.dest, e.weight+cur[1]});
+                    pq.offer(new int[]{e.dest, e.weight + cur[1]});
                 }
             }
-            if(i==N-2) res = cur[1];
+            if(i == N - 2) res = cur[1];
         }
         return res;
     }
