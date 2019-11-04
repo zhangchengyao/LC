5cc1ba173943d88e64fa1e0a5b3ea604543d7809
chengyaoZhang <tc2002618@126.com>
2019-11-03 16:31:49 -0800

    LC 736 modify

--- src/LC736_ParseLispExpression.java
+++ src/LC736_ParseLispExpression.java
@@ -1,62 +1,55 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LC736_ParseLispExpression {
     public int evaluate(String expression) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         return evaluate(expression, map);
     }
-    private int evaluate(String expression, HashMap<String, Integer> map){
-        if(expression.charAt(0)=='('){
-            if(expression.substring(1, 4).equals("let")){
-                List<String> exprs = parse(expression.substring(5));
-                for(int i=0;i<exprs.size()-1;i+=2){
-                    map.put(exprs.get(i), evaluate(exprs.get(i+1), new HashMap<>(map)));
+
+    private int evaluate(String expression, Map<String, Integer> map) {
+        if(expression.charAt(0) == '(') {
+            if(expression.substring(1, 4).equals("let")) {
+                List<String> exprs = parse(expression.substring(5, expression.length() - 1));
+                for(int i = 0; i < exprs.size() - 1; i += 2){
+                    map.put(exprs.get(i), evaluate(exprs.get(i + 1), new HashMap<>(map)));
                 }
-                return evaluate(exprs.get(exprs.size()-1), new HashMap<>(map));
-            }
-            else if(expression.substring(1, 4).equals("add")){
-                List<String> exprs = parse(expression.substring(5));
+                return evaluate(exprs.get(exprs.size() - 1), new HashMap<>(map));
+            } else if(expression.substring(1, 4).equals("add")) {
+                List<String> exprs = parse(expression.substring(5, expression.length() - 1));
                 return evaluate(exprs.get(0), new HashMap<>(map)) + evaluate(exprs.get(1), new HashMap<>(map));
-            }
-            else{
-                List<String> exprs = parse(expression.substring(6));
+            } else {
+                List<String> exprs = parse(expression.substring(6, expression.length() - 1));
                 return evaluate(exprs.get(0), new HashMap<>(map)) * evaluate(exprs.get(1), new HashMap<>(map));
             }
-        }
-        else{
-            if((expression.charAt(0)>='0'&&expression.charAt(0)<='9')||expression.charAt(0)=='-'){
-                int p = 1;
-                while(p<expression.length() && expression.charAt(p)!=' ' && expression.charAt(p)!=')') p++;
-                return Integer.parseInt(expression.substring(0, p));
-            }
-            else{
-                int p = 1;
-                while(p<expression.length() && expression.charAt(p)!=' ' && expression.charAt(p)!=')') p++;
-                return map.get(expression.substring(0, p));
+        } else {
+            if(Character.isDigit(expression.charAt(0)) || expression.charAt(0) == '-') {
+                return Integer.parseInt(expression);
+            } else {
+                return map.get(expression);
             }
         }
     }
-    private List<String> parse(String expression){
+    private List<String> parse(String expression) {
         List<String> res = new ArrayList<>();
         int cur = 0;
-        while(cur<expression.length()){
-            int p = cur+1;
-            if(expression.charAt(cur)=='('){
+        while(cur < expression.length()) {
+            int p = cur + 1;
+            if(expression.charAt(cur) == '(') {
                 int cnt = 1;
-                while(p<expression.length()){
-                    if(expression.charAt(p)=='(') cnt++;
-                    else if(expression.charAt(p)==')') cnt--;
+                while(p < expression.length()) {
+                    if(expression.charAt(p) == '(') cnt++;
+                    else if(expression.charAt(p) == ')') cnt--;
                     p++;
-                    if(cnt==0) break;
+                    if(cnt == 0) break;
                 }
-            }
-            else{
-                while(p<expression.length() && expression.charAt(p)!=' ') p++;
+            } else {
+                while(p < expression.length() && expression.charAt(p) != ' ') p++;
             }
             res.add(expression.substring(cur, p));
-            cur = p+1;
+            cur = p + 1;
         }
         return res;
     }
