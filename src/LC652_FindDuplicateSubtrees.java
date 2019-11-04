89a36ba98ea9e015865a5219a5168c15e95285c6
chengyaoZhang <tc2002618@126.com>
2019-11-04 11:43:15 -0800

    LC 652 modify

--- src/LC652_FindDuplicateSubtrees.java
+++ src/LC652_FindDuplicateSubtrees.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LC652_FindDuplicateSubtrees {
     class TreeNode{
@@ -9,21 +10,23 @@ public class LC652_FindDuplicateSubtrees {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         List<TreeNode> res = new ArrayList<>();
         preOrder(map, root, res);
         return res;
     }
-    private String preOrder(HashMap<String, Integer> map, TreeNode root, List<TreeNode> res){
-        if(root==null) return "null";
+
+    private String preOrder(Map<String, Integer> map, TreeNode root, List<TreeNode> res){
+        if(root == null) return "null";
         StringBuilder sb = new StringBuilder();
-        sb.append(root.val+" ");
+        sb.append(root.val).append(" ");
         sb.append(preOrder(map, root.left, res));
         sb.append(preOrder(map, root.right, res));
         String s = sb.toString();
-        if(map.containsKey(s) && map.get(s)==1) res.add(root);
-        map.put(s, map.getOrDefault(s, 0)+1);
+        if(map.containsKey(s) && map.get(s) == 1) res.add(root);
+        map.put(s, map.getOrDefault(s, 0) + 1);
         return sb.toString();
     }
 }
