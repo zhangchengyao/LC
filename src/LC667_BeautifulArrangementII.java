e3e4d28cb06829a0629e85523beb4a6ef5306138
chengyaoZhang <tc2002618@126.com>
2019-11-05 14:36:13 -0800

    LC 667 modify

--- src/LC667_BeautifulArrangementII.java
+++ src/LC667_BeautifulArrangementII.java
@@ -1,16 +1,16 @@
 public class LC667_BeautifulArrangementII {
     public int[] constructArray(int n, int k) {
         int[] res = new int[n];
-        int s = 1;
-        int b = n;
+        int l = 1;
+        int r = n;
         int i = 0;
-        for(;i<k-1;i++){
-            res[i] = (i%2==0?s++:b--);
+        for(; i < k - 1; i++) {
+            res[i] = (i % 2 == 0 ? l++: r--);
         }
-        if(i%2==0){
-            while(s<=b) res[i++] = s++;
-        }else{
-            while(s<=b) res[i++] = b--;
+        if(i % 2 == 0) {
+            while(l <= r) res[i++] = l++;
+        } else {
+            while(l <= r) res[i++] = r--;
         }
         return res;
     }
