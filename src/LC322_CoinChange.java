3d37dd5976c209a787f0f061caf0b04f4c4da1be
chengyaoZhang <tc2002618@126.com>
2019-11-05 13:04:03 -0800

    LC 322 modify

--- src/LC322_CoinChange.java
+++ src/LC322_CoinChange.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class LC322_CoinChange {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        for(int i=1;i<dp.length;i++) dp[i] = -1;
-        for(int i=1;i<dp.length;i++){
-            for(int j=0;j<coins.length;j++){
-                if(i-coins[j]>=0 && dp[i-coins[j]]!=-1){
-                    dp[i] = dp[i]==-1?dp[i-coins[j]]+1:Math.min(dp[i], dp[i-coins[j]]+1);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i = 1; i < dp.length; i++){
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount];
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
