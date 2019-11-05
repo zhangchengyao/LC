7defedb5abef1f826b0ccf87ab582c8e34a83852
chengyaoZhang <tc2002618@126.com>
2019-11-05 14:17:20 -0800

    LC 526 modify

--- src/LC526_BeautifulArrangement.java
+++ src/LC526_BeautifulArrangement.java
@@ -1,25 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class LC526_BeautifulArrangement {
     public int countArrangement(int N) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=N;i++) list.add(i);
-        return count(1, list);
+        int[] nums = new int[N];
+        for(int i = 1; i <= N; i++) nums[i - 1] = i;
+        return count(nums, 0);
     }
-    private int count(int start, List<Integer> list){
-        if(list.size()==1){
-            if(start%list.get(0)==0 || list.get(0)%start==0) return 1;
+    private int count(int[] nums, int cur) {
+        if(cur == nums.length - 1) {
+            if((cur + 1) % nums[cur] == 0 || nums[cur] % (cur + 1) == 0) return 1;
             else return 0;
         }
         int res = 0;
-        for(int i=0;i<list.size();i++){
-            if(start%list.get(i)==0 || list.get(i)%start==0){
-                int cur = list.remove(i);
-                res += count(start+1, list);
-                list.add(i, cur);
+        for(int i = cur; i < nums.length; i++){
+            if((cur + 1) % nums[i] == 0 || nums[i] % (cur + 1) == 0){
+                swap(nums, cur, i);
+                res += count(nums, cur + 1);
+                swap(nums, cur, i);
             }
         }
         return res;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
