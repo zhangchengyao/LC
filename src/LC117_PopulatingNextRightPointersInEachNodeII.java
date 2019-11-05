37817324c538b1b4423f2840b44343b37f95b69a
chengyaoZhang <tc2002618@126.com>
2019-11-04 22:12:33 -0800

    LC 117 modify

--- src/LC117_PopulatingNextRightPointersInEachNodeII.java
+++ src/LC117_PopulatingNextRightPointersInEachNodeII.java
@@ -1,53 +1,37 @@
 public class LC117_PopulatingNextRightPointersInEachNodeII {
-    public class TreeLinkNode {
+    public class Node {
         int val;
-        TreeLinkNode left, right, next;
-        TreeLinkNode(int x) { val = x; }
+        Node left, right, next;
+        Node(int x) { val = x; }
     }
-    public void connect(TreeLinkNode root) {
-        if(root==null) return;
-        TreeLinkNode cur = root;
-        while(cur!=null && cur.left==null && cur.right==null) cur = cur.next;
-        if(cur==null) return;
-        TreeLinkNode child = null;
-        if(cur.left==null){
-            child = cur.right;
-        }
-        else if(cur.right==null){
-            child = cur.left;
-        }
-        else{
-            cur.left.next = cur.right;
-            child = cur.right;
-        }
-        root = cur.left==null?cur.right:cur.left;
-        TreeLinkNode after = cur.next;
-        TreeLinkNode afterChild = null;
-        while(after!=null){
-            if(after.left==null && after.right==null) after = after.next;
-            else if(after.left==null){
-                afterChild = after.right;
-                child.next = afterChild;
-                cur = after;
-                after = cur.next;
-                child = afterChild;
-            }
-            else if(after.right==null){
-                afterChild = after.left;
-                child.next = afterChild;
-                cur = after;
-                after = cur.next;
-                child = afterChild;
-            }
-            else{
-                afterChild = after.left;
-                child.next = afterChild;
-                cur = after;
-                after = cur.next;
+
+    public Node connect(Node root) {
+        Node cur = root;
+        while(cur != null && cur.left == null && cur.right == null) cur = cur.next;
+        if(cur == null) return root;
+
+        Node head = cur.left == null ? cur.right : cur.left;
+
+        Node after = cur.next;
+        while(true) {
+            while(after != null && after.left == null && after.right == null) after = after.next;
+            if(after == null) break;
+
+            if(cur.left != null && cur.right != null) {
                 cur.left.next = cur.right;
-                child = cur.right;
+                cur.right.next = after.left == null ? after.right : after.left;
+            } else if(cur.right == null) {
+                cur.left.next = after.left == null ? after.right : after.left;
+            } else {
+                cur.right.next = after.left == null ? after.right : after.left;
             }
+            cur = after;
+            after = after.next;
         }
-        connect(root);
+        if(cur.left != null && cur.right != null) cur.left.next = cur.right;
+
+        connect(head);
+
+        return root;
     }
 }
