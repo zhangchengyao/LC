8e139363f4593461f5beef381a38c4bfdca4dd7c
chengyaoZhang <tc2002618@126.com>
2019-11-05 13:42:37 -0800

    LC 655 modify

--- src/LC665_NonDecreasingArray.java
+++ src/LC665_NonDecreasingArray.java
@@ -1,16 +1,16 @@
 public class LC665_NonDecreasingArray {
     public boolean checkPossibility(int[] nums) {
         int local = 0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]>nums[i]){
-                if(i==1) nums[i-1] = nums[i];
-                else{
-                    if(nums[i-2]<nums[i]) nums[i-1] = nums[i];
-                    else nums[i] = nums[i-1];
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i - 1] > nums[i]) {
+                if(i == 1) nums[i - 1] = nums[i];
+                else {
+                    if(nums[i - 2] < nums[i]) nums[i - 1] = nums[i];
+                    else nums[i] = nums[i - 1];
                 }
                 local++;
             }
-            if(local>1) return false;
+            if(local > 1) return false;
         }
         return true;
     }
