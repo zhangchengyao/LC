a5f1fa05419babf6b7e729c44be92f37b579cc58
chengyaoZhang <tc2002618@126.com>
2019-11-05 22:53:25 -0800

    LC 676 modify

--- src/LC676_ImplementMagicDictionary.java
+++ src/LC676_ImplementMagicDictionary.java
@@ -1,8 +1,9 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class LC676_ImplementMagicDictionary {
-    private HashSet<String> set;
-    private HashSet<String> oriSet;
+    private Set<String> set;
+    private Set<String> oriSet;
     /** Initialize your data structure here. */
     public LC676_ImplementMagicDictionary() {
         set = new HashSet<>();
@@ -11,15 +12,15 @@ public class LC676_ImplementMagicDictionary {
 
     /** Build a dictionary through a list of words */
     public void buildDict(String[] dict) {
-        for(String str: dict){
+        for(String str: dict) {
             boolean found = false;
-            for(int i=0;i<str.length();i++){
-                String tmp = str.substring(0, i)+"*"+str.substring(i+1);
+            for(int i = 0; i < str.length(); i++) {
+                String tmp = str.substring(0, i) + "*" + str.substring(i + 1);
                 if(set.contains(tmp)){
                     found = true;
-                    for(int k=0;k<26;k++){
-                        String word = str.substring(0, i)+(char)('a'+k)+str.substring(i+1);
-                        if(oriSet.contains(word)){
+                    for(int k = 0; k < 26; k++){
+                        String word = str.substring(0, i) + (char)('a' + k) + str.substring(i + 1);
+                        if(oriSet.contains(word)) {
                             oriSet.remove(word);
                             break;
                         }
@@ -34,8 +35,8 @@ public class LC676_ImplementMagicDictionary {
     /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
     public boolean search(String word) {
         if(oriSet.contains(word)) return false;
-        for(int i=0;i<word.length();i++){
-            if(set.contains(word.substring(0, i)+"*"+word.substring(i+1))) return true;
+        for(int i = 0; i < word.length(); i++) {
+            if(set.contains(word.substring(0, i) + "*" + word.substring(i + 1))) return true;
         }
         return false;
     }
