629921f640998e5994bb2158e20778230aa573d9
chengyaoZhang <tc2002618@126.com>
2019-11-05 22:23:29 -0800

    LC 674 modify

--- src/LC674_LongestContinuousIncreasingSubsequence.java
+++ src/LC674_LongestContinuousIncreasingSubsequence.java
@@ -1,12 +1,12 @@
 public class LC674_LongestContinuousIncreasingSubsequence {
     public int findLengthOfLCIS(int[] nums) {
-        if(nums==null || nums.length==0) return 0;
+        if(nums == null || nums.length == 0) return 0;
         int max = 1;
         int cur = 1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[i - 1]) {
                 cur++;
-            }else{
+            } else {
                 max = Math.max(max, cur);
                 cur = 1;
             }
