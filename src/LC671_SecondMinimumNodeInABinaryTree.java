59bc7c2d620a4fa8080e7553ac54f8fda5f8b96b
chengyaoZhang <tc2002618@126.com>
2019-11-05 16:28:40 -0800

    LC 671 modify

--- src/LC671_SecondMinimumNodeInABinaryTree.java
+++ src/LC671_SecondMinimumNodeInABinaryTree.java
@@ -6,12 +6,14 @@ public class LC671_SecondMinimumNodeInABinaryTree {
         TreeNode(int x){ val = x; }
     }
     public int findSecondMinimumValue(TreeNode root) {
-        if(root.left==null) return -1;
+        if(root.left == null) return -1;
+
         int min = root.val;
-        int findLeft = root.left.val==min?findSecondMinimumValue(root.left):root.left.val;
-        int findRight = root.right.val==min?findSecondMinimumValue(root.right):root.right.val;
-        if(findLeft==-1) return findRight;
-        if(findRight==-1) return findLeft;
-        return Math.min(findLeft, findRight);
+        int leftRes = root.left.val == min ? findSecondMinimumValue(root.left) : root.left.val;
+        int rightRes = root.right.val == min ? findSecondMinimumValue(root.right) : root.right.val;
+        if(leftRes == -1) return rightRes;
+        if(rightRes == -1) return leftRes;
+
+        return Math.min(leftRes, rightRes);
     }
 }
