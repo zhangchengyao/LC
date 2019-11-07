72e81d31815fe3f328f49554e00c98d646f96b9a
chengyaoZhang <tc2002618@126.com>
2019-11-07 13:25:38 -0800

    LC 681 modify

--- src/LC681_NextClosestTime.java
+++ src/LC681_NextClosestTime.java
@@ -6,18 +6,17 @@ public class LC681_NextClosestTime {
         TreeSet<Character> set = new TreeSet<>();
         for(char c: digits) set.add(c);
 
-        for(int i = digits.length - 1; i >= 0; i--){
+        for(int i = digits.length - 1; i >= 0; i--) {
             if(set.higher(digits[i]) != null){
                 char c = set.higher(digits[i]);
                 if(i == 2 && c >= '6') continue;
                 if(i == 1 && time.charAt(0) == '2' && c >= '4') continue;
                 if(i == 0 && c >= '3') continue;
                 StringBuilder res = new StringBuilder(time);
-                if(i > 1){
+                if(i > 1) {
                     res.setCharAt(++i, c);
                     while(++i < time.length()) res.setCharAt(i, set.first());
-                }
-                else{
+                } else {
                     res.setCharAt(i, c);
                     while(++i < time.length())
                         if(time.charAt(i) != ':')
@@ -27,6 +26,6 @@ public class LC681_NextClosestTime {
             }
         }
         char c = set.first();
-        return String.format("%d%d:%d%d", c, c, c, c);
+        return String.format("%c%c:%c%c", c, c, c, c);
     }
 }
