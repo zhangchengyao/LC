b35dd32da12eb513144796a0554517e1a3bc3ebc
chengyaoZhang <tc2002618@126.com>
2019-11-07 20:43:50 -0800

    LC 684 modify

--- src/LC684_RedundantConnection.java
+++ src/LC684_RedundantConnection.java
@@ -1,24 +1,24 @@
 public class LC684_RedundantConnection {
     public int[] findRedundantConnection(int[][] edges) {
         int[] parent = new int[edges.length];
-        for(int i=0;i<parent.length;i++) parent[i] = i;
-        for(int i=0;i<edges.length;i++){
-            if(!union(edges[i][0]-1, edges[i][1]-1, parent)) return new int[]{edges[i][0],edges[i][1]};
+        for(int i = 0; i < parent.length; i++) parent[i] = i;
+        for (int[] edge : edges) {
+            if (!union(edge[0] - 1, edge[1] - 1, parent)) return new int[]{edge[0], edge[1]};
         }
         return null;
     }
-    private int find(int i, int[] arr){
-        while(arr[i]!=i){
-            arr[i] = arr[arr[i]];
-            i = arr[i];
+    private int find(int i, int[] parent) {
+        while(parent[i] != i) {
+            parent[i] = parent[parent[i]];
+            i = parent[i];
         }
         return i;
     }
-    private boolean union(int i, int j, int[] arr){
-        int root1 = find(i, arr);
-        int root2 = find(j, arr);
-        if(root1==root2) return false;
-        arr[root1] = root2;
+    private boolean union(int i, int j, int[] parent){
+        int root1 = find(i, parent);
+        int root2 = find(j, parent);
+        if(root1 == root2) return false;
+        parent[root1] = root2;
         return true;
     }
 }
