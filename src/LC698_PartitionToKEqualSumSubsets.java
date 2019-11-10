16b8b353fe8e51ef7b8d29a0eeee972645da7483
chengyaoZhang <tc2002618@126.com>
2019-11-10 10:55:02 -0800

    LC 698 modify

--- src/LC698_PartitionToKEqualSumSubsets.java
+++ src/LC698_PartitionToKEqualSumSubsets.java
@@ -1,22 +1,22 @@
 public class LC698_PartitionToKEqualSumSubsets {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum += nums[i];
+        for (int num : nums) {
+            sum += num;
         }
-        if(sum%k!=0) return false;
-        int target = sum/k;
+        if(sum % k != 0) return false;
+        int target = sum / k;
         boolean[] visited = new boolean[nums.length];
         return partitionRec(nums, k, target, 0, 0, visited);
     }
-    private boolean partitionRec(int[] nums, int k, int target, int start, int curSum, boolean[] visited){
-        if(k==1) return true;
-        if(curSum>target) return false;
-        if(curSum==target) return partitionRec(nums, k-1, target, 0, 0, visited);
-        for(int i=start;i<nums.length;i++){
-            if(!visited[i]){
+    private boolean partitionRec(int[] nums, int k, int target, int start, int curSum, boolean[] visited) {
+        if(k == 1) return true;
+        if(curSum > target) return false;
+        if(curSum == target) return partitionRec(nums, k - 1, target, 0, 0, visited);
+        for(int i = start; i < nums.length; i++) {
+            if(!visited[i]) {
                 visited[i] = true;
-                boolean res = partitionRec(nums, k, target, i+1, curSum+nums[i], visited);
+                boolean res = partitionRec(nums, k, target, i + 1, curSum + nums[i], visited);
                 if(!res) visited[i] = false;
                 else return true;
             }
