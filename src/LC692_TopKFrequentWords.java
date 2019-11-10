bad5b0b66b31ccdd953ef0e95bd74eaad9d49110
chengyaoZhang <tc2002618@126.com>
2019-11-09 19:37:45 -0800

    LC 692 modify

--- src/LC692_TopKFrequentWords.java
+++ src/LC692_TopKFrequentWords.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class LC692_TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for(String word: words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
