dfd41d0da4d3769f3c1a427574192440c62a20f9
chengyaoZhang <tc2002618@126.com>
2019-11-11 12:05:27 -0800

    LC 606 modify

--- src/LC606_ConstructStringFromBinaryTree.java
+++ src/LC606_ConstructStringFromBinaryTree.java
@@ -5,11 +5,12 @@ public class LC606_ConstructStringFromBinaryTree {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+
     public String tree2str(TreeNode t) {
-        if(t==null) return "";
-        if(t.left==null && t.right==null) return t.val+"";
-        if(t.right==null) return t.val+"("+tree2str(t.left)+")";
-        if(t.left==null) return t.val+"()("+tree2str(t.right)+")";
-        return t.val+"("+tree2str(t.left)+")"+"("+tree2str(t.right)+")";
+        if(t == null) return "";
+        if(t.left == null && t.right == null) return t.val + "";
+        if(t.right == null) return t.val + "(" + tree2str(t.left) + ")";
+        if(t.left == null) return t.val + "()(" + tree2str(t.right) + ")";
+        return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
     }
 }
