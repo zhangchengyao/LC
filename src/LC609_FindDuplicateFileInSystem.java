d9e64b9c941713cec84dc10c91cb3c17f8adef7e
chengyaoZhang <tc2002618@126.com>
2019-11-13 13:35:12 -0800

    LC 609 modify

--- src/LC609_FindDuplicateFileInSystem.java
+++ src/LC609_FindDuplicateFileInSystem.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LC609_FindDuplicateFileInSystem {
     public List<List<String>> findDuplicate(String[] paths) {
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for(String path: paths) {
             String[] info = path.split(" ");
             String dir = info[0];
