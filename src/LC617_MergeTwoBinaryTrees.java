928e70f4718aa08badcd2bc962d0f705ec40a47e
chengyaoZhang <tc2002618@126.com>
2019-11-20 10:26:53 -0800

    LC 617 modify

--- src/LC617_MergeTwoBinaryTrees.java
+++ src/LC617_MergeTwoBinaryTrees.java
@@ -5,10 +5,11 @@ public class LC617_MergeTwoBinaryTrees {
         TreeNode right;
         TreeNode(int x){ val = x; }
     }
+
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1==null) return t2;
-        if(t2==null) return t1;
-        TreeNode newRoot = new TreeNode(t1.val+t2.val);
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+        TreeNode newRoot = new TreeNode(t1.val + t2.val);
         newRoot.left = mergeTrees(t1.left, t2.left);
         newRoot.right = mergeTrees(t1.right, t2.right);
         return newRoot;
