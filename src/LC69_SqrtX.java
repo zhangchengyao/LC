6246da5dc4591079e4eecac005947a105cf8ab60
chengyaoZhang <tc2002618@126.com>
2021-05-30 20:43:51 -0700

    Update LC69

--- src/LC69_SqrtX.java
+++ src/LC69_SqrtX.java
@@ -1,16 +1,18 @@
 public class LC69_SqrtX {
     public int mySqrt(int x) {
+        if (x == 0) return x;
         int left = 1;
         int right = x;
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(x/mid==mid) return mid;
-            else if(x/mid<mid) right = mid-1;
-            else left = mid+1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (x / mid == mid) return mid;
+            else if (x / mid < mid) right = mid - 1;
+            else left = mid + 1;
         }
         return right;
+
         // long res = x;
-        // while(res*res>x) res = (res+x/res)/2;
-        // return (int)res;
+        // while (res * res > x) res = (res + x / res) / 2;
+        // return (int) res;
     }
 }
