b3b648f502189fab175a47e636beac52a89a9ca5
chengyaoZhang <tc2002618@126.com>
2021-06-01 23:00:31 -0700

    Update LC153

--- src/LC153_FindMinimumInRotatedSortedArray.java
+++ src/LC153_FindMinimumInRotatedSortedArray.java
@@ -1,10 +1,14 @@
 public class LC153_FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                return nums[i+1];
-            }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) return nums[left];
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) right = mid;
+            else left = mid + 1;
         }
-        return nums[0];
+        
+        return nums[left];
     }
 }
