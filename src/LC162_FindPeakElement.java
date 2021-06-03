30e0cec74b4f000e6022d48219f9c94a49889545
chengyaoZhang <tc2002618@126.com>
2021-06-02 20:46:45 -0700

    Update LC162

--- src/LC162_FindPeakElement.java
+++ src/LC162_FindPeakElement.java
@@ -1,21 +1,13 @@
 public class LC162_FindPeakElement {
-    // [1,2,3,2,3,4,4,4,4]
     public int findPeakElement(int[] nums) {
-        return find(nums, 0, nums.length-1);
+        return search(nums, 0, nums.length);
     }
-    private int find(int[] nums, int l, int r){
-        if(l>r) return -1;
-        if(l==r) return l;
-        int mid = l+(r-l)/2;
-        if((mid==l || nums[mid]>nums[mid-1]) && (mid==r||nums[mid]>nums[mid+1])) return mid;
-        int res = -1;
-        int cur = mid;
-        while(cur>l && nums[cur]>=nums[cur-1]) cur--;
-        if(cur!=l) res = find(nums, l, cur-1);
-        if(res!=-1) return res;
-        cur = mid;
-        while(cur<r && nums[cur]>=nums[cur+1]) cur++;
-        if(cur!=r) res = find(nums, cur+1, r);
-        return res;
+    
+    private int search(int[] nums, int left, int right) {
+        if (left == right - 1) return left;
+        int mid = left + (right - left) / 2;
+        int mid2 = mid - 1;
+        if (nums[mid2] < nums[mid]) return search(nums, mid, right);
+        else return search(nums, left, mid);
     }
 }
