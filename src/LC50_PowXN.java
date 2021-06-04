a8470c80cc4a88d33a9d8dd087b293514d154ea2
chengyaoZhang <tc2002618@126.com>
2021-06-03 20:30:49 -0700

    Update LC50

--- src/LC50_PowXN.java
+++ src/LC50_PowXN.java
@@ -1,17 +1,9 @@
 public class LC50_PowXN {
     public double myPow(double x, int n) {
-        if(n==0 || x==1) return 1.0;
-        if(x==0) return 0.0;
-        if(n<0) return 1/(x*powRec(x, -(n+1)));
-        else return powRec(x, n);
-    }
-    private double powRec(double x, int n){
-        if(n==0 || x==1.0) return 1.0;
-        if(x==0.0) return 0.0;
-        double half = powRec(x, n/2);
-        double res;
-        if((n&1)==1) res = half*half*x;
-        else res = half*half;
-        return res;
+        if (n == 0 || x == 1) return 1;
+        if (n < 0) return 1 / (x * myPow(x, -(n + 1)));
+        double half = myPow(x, n / 2);
+        if ((n & 1) == 1) return x * half * half;
+        else return half * half;
     }
 }
